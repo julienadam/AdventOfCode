9fe4320bba07047d6e86433046dbbc28da766972
Julien Adam <julien.adam@gmail.com>
2021-12-19 18:35:12 +0100

    Missing day 18 files, parsing done for day 19

---
+++ Puzzles/2021/Day19.fsx
@@ -0,0 +1,25 @@
+﻿open System.Diagnostics
+open System
+
+#load "../../Tools.fsx"
+
+open Tools
+open System.IO
+
+let mapPosLine (l:string) =
+    l.Split(",") 
+    |> Array.map (fun l -> 
+        let r, i = Int32.TryParse(l)
+        if not r then
+            failwithf "Invalid input %s" l
+        else
+            i
+        ) 
+        |> Array.toList
+
+let inputStr = getInputPath "Day19.txt" |> File.ReadAllText
+inputStr.Split("\r\n\r\n")
+|> Array.map (fun lines -> lines.Split("\r\n") |> Array.skip 1)
+|> Array.map (fun positions -> positions |> Array.map mapPosLine |> Array.toList)
+|> Array.toList
+|> Dump
