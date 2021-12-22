6952a2e1566fe31005f33ceafaac49ff25e3308f
Julien Adam <julien.adam@gmail.com>
2021-12-22 14:41:34 +0100

    WIP, Set -> Map

--- Puzzles/2021/Day20.fsx
+++ Puzzles/2021/Day20.fsx
@@ -6,14 +6,13 @@ open System.Diagnostics
 open System.IO
 open Tools
 
-let a = [[1;2;3;4;5];[6;7;8;9;10]] |> array2D
-a |> Dump
-a.[1,2] |> Dump
-
 let getInput fileName = 
     let inline mapPixel c = match c with | '#' -> 1 | '.' -> 0 | _ -> failwithf "Invalid input char %c" c
     let lines = File.ReadAllLines (getInputPath fileName)
     let enhancement = lines.[0] |> Seq.map mapPixel |> Seq.toList
+
+    assert(enhancement.Length = 512)
+
     let inputImage =
         lines 
         |> Seq.skip 2 
@@ -26,24 +25,107 @@ let getInput fileName =
 
     enhancement, inputImage
 
-let enhancement, image = getInput "Day20_Sample1.txt"
+let inline getPixelAt (row:int) (col:int) defaultVal (image: Map<int*int, int>)  =
+    match image.TryFind (row, col) with
+    | Some i -> i
+    | None -> defaultVal
 
-let inline getPixelAt (row:int) (col:int) (image: Set<int*int>) =
-    if image |> Set.contains (row, col) then 1 else 0
+let getPixelsAround (row:int) (col:int) (image: Map<int*int, int>) defaultVal = seq {
 
-let getPixelsAround (row:int) (col:int) (image: Set<int*int>) = seq {
-
-    yield image |> getPixelAt (row - 1) (col - 1)
-    yield image |> getPixelAt (row - 1) col
-    yield image |> getPixelAt (row - 1) (col + 1)
-    yield image |> getPixelAt row (col - 1)
-    yield image |> getPixelAt row col
-    yield image |> getPixelAt row (col + 1)
-    yield image |> getPixelAt (row + 1) (col - 1)
-    yield image |> getPixelAt (row + 1) col
-    yield image |> getPixelAt (row + 1) (col + 1)
+    yield image |> getPixelAt (row - 1) (col - 1) defaultVal
+    yield image |> getPixelAt (row - 1) col defaultVal
+    yield image |> getPixelAt (row - 1) (col + 1) defaultVal
+    yield image |> getPixelAt row (col - 1) defaultVal
+    yield image |> getPixelAt row col defaultVal
+    yield image |> getPixelAt row (col + 1) defaultVal
+    yield image |> getPixelAt (row + 1) (col - 1) defaultVal
+    yield image |> getPixelAt (row + 1) col defaultVal
+    yield image |> getPixelAt (row + 1) (col + 1) defaultVal
 }
 
 let boolsToIntRev bools = bools |> Array.rev |> Array.mapi (fun i b -> b <<< i) |> Array.sum
-getPixelsAround 2 2 image |> Seq.toArray |> boolsToIntRev
+//getPixelsAround 2 2 image |> Seq.toArray |> boolsToIntRev
 
+let getBoundsOfNextImage (image:Map<int*int, int>) =
+    let xs = image |> Map.keys |> Seq.map fst |> Seq.toList
+    let ys = image |> Map.keys |> Seq.map snd |> Seq.toList
+    (((xs |> Seq.min) - 2), ((xs |> Seq.max) + 2)), (((ys |> Seq.min) - 2), ((ys |> Seq.max) + 2)) 
+
+let getDefaultValueForStep step (enhancement:int list) =
+    match step % 2, enhancement.[0] with
+    | _, 0 -> 0
+    | 1, _ -> enhancement.[0]
+    | 0, _ -> enhancement.[511]
+    | _ -> failwithf "unsupported default value situation"
+
+
+let buildNextImage image step (enhancement:int list) = seq {
+    let ((minR, maxR), (minC, maxC)) = getBoundsOfNextImage image
+    printfn "New bounds : row [%i to %i], col [%i to %i]"  minR maxR minC maxC
+
+    let defaultVal = getDefaultValueForStep step enhancement
+
+    for r = minR to maxR do
+        for c = minC to maxC do
+            let enhanceIndex = getPixelsAround r c image defaultVal |> Seq.toArray |> boolsToIntRev
+            yield (r, c, enhancement.[enhanceIndex])
+}
+
+let printImage image step enhancement =
+    let ((minR, maxR), (minC, maxC)) = getBoundsOfNextImage image
+
+    let defaultVal = getDefaultValueForStep step enhancement
+    
+    for r = minR to maxR do
+        for c = minC to maxC do
+            match getPixelAt r c defaultVal image with
+            | 1 -> printf "#"
+            | 0 -> printf "."
+            | i -> failwithf "invalid pixel value %i" i
+        printfn ""
+            //let enhanceIndex = getPixelsAround r c image |> Seq.toArray |> boolsToIntRev
+            //yield (r, c, enhancement.[enhanceIndex])
+
+
+let solve1 fileName =
+    let enhancement, image = getInput fileName
+    let mutable step = 0
+    
+    let defaultVal = getDefaultValueForStep step enhancement
+    printfn "Default val %i" defaultVal
+    printImage image step enhancement
+
+    let afterStep1 = 
+        buildNextImage image step enhancement
+        |> Seq.filter (fun (_,_,v) -> v = 1)
+        |> Seq.map (fun (r,c,_) -> (r,c))
+        |> Set.ofSeq
+    
+    step <- step + 1
+
+    printImage afterStep1 step enhancement
+    printfn "After step 1 : %i pixels lit" afterStep1.Count
+
+    let afterStep2 =
+        buildNextImage afterStep1 step enhancement
+        |> Seq.filter (fun (_,_,v) -> v = 1)
+        |> Seq.map (fun (r,c,_) -> (r,c))
+        |> Set.ofSeq
+    
+    step <- step + 1
+   
+    printImage afterStep2 step enhancement
+    printfn "After step 1 : %i pixels lit" afterStep2.Count
+    
+    afterStep2.Count |> Dump
+
+
+let enhancement, image = getInput "Day20.txt"
+
+
+getPixelsAround -1 -1 image |> Seq.toArray |> boolsToIntRev |> Dump
+enhancement.[0]
+
+solve1 "Day20_Sample1.txt"
+
+solve1 "Day20.txt"
