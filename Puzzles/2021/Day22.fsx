e6a244931585f7737245929680fcbe0685a45dc1
Julien Adam <julien.adam@gmail.com>
2021-12-24 16:46:33 +0100

    Day 22 : implemented cuboid intersection function

--- Puzzles/2021/Day22.fsx
+++ Puzzles/2021/Day22.fsx
@@ -59,22 +59,50 @@ let sw = Stopwatch.StartNew()
 let result = solve1 "Day22.txt"
 printfn "Day 22 Part 1 solution : %i. Took %A" result sw.Elapsed
 
+let intersect (((xs1,xe1),(ys1,ye1),(zs1,ze1)):Cuboid) (((xs2,xe2),(ys2,ye2),(zs2,ze2)):Cuboid) : Cuboid option =
+    let xsr = max xs1 xs2
+    let xer = min xe1 xe2
+    if xsr > xer then
+        None
+    else
+        let ysr = max ys1 ys2
+        let yer = min ye1 ye2
+        if ysr > yer then 
+            None
+        else
+            let zsr = max zs1 zs2
+            let zer = min ze1 ze2
+            if zsr > zer then
+                None
+            else
+                Some ((xsr, xer),(ysr,yer),(zsr,zer))
+
+assert(intersect ((0,1),(0,1),(0,1)) ((0,2),(0,2),(0,2)) = Some ((0,1),(0,1),(0,1)))
+assert(intersect ((0,2),(0,2),(0,2)) ((0,1),(0,1),(0,1)) = Some ((0,1),(0,1),(0,1)))
+assert(intersect ((-1,2),(-1,2),(-1,2)) ((0,2),(0,2),(0,2)) = Some ((0,2),(0,2),(0,2)))
+assert(intersect ((-1,0),(-1,0),(-1,0)) ((0,2),(0,2),(0,2)) = Some ((0,0),(0,0),(0,0)))
+assert(intersect ((-2,2),(-2,2),(-2,2)) ((-1,1),(-1,1),(-1,1)) = Some ((-1,1),(-1,1),(-1,1)))
+assert(intersect ((-2,2),(-2,2),(-2,2)) ((5,5),(-1,1),(-1,1)) = None)
+assert(intersect ((-2,2),(-2,2),(-2,2)) ((-1,1),(-5,-5),(-1,1)) = None)
+assert(intersect ((-2,2),(-2,2),(-2,2)) ((-1,1),(-1,-1),(6,6)) = None)
+
 
 let solve2 fileName = 
-    let input = getInputPath fileName |> File.ReadAllLines |> Seq.map mapLine |> Seq.toList
-    let totalInstructions = input.Length
-    let grid = new Dictionary<(int * int * int), bool>()
-    
-    input
-    |> Seq.iteri(fun i ((xr,yr,zr), state) -> 
-        printfn "Iteration %i of %i" i totalInstructions
-        for x in enumRange xr do
-            for y in enumRange yr do
-                for z in enumRange zr do
-                    grid.[(x,y,z)] <- state
-    )
+    // Idea 1 : 
+    //  take the cubes by pairs
+    //  keep non-intersecting cubes
+    //  if intersecting, cut them up in smaller, non-intersecting cubes 
+    //  take all of these and sum the volumes of the ON ones ?
 
-    grid.Values |> Seq.filter id |> Seq.length
+    // Idea 2 :
+    // brute force each pixel, traversing each cube
+
+    let input = getInputPath fileName |> File.ReadAllLines |> Seq.map mapLine |> Seq.toList
+    let (xs,xe) = input |> Seq.map (fun (((xs,_), _, _), _) -> xs) |> Seq.min, input |> Seq.map (fun (((_,xe), _, _), _) -> xe) |> Seq.max
+    let (ys,ye) = input |> Seq.map (fun ((_, (ys,_), _), _) -> ys) |> Seq.min, input |> Seq.map (fun ((_, (_,ye), _), _) -> ye) |> Seq.max
+    let (zs,ze) = input |> Seq.map (fun ((_, _, (zs,_)), _) -> zs) |> Seq.min, input |> Seq.map (fun ((_, _, (_,ze)), _) -> ze) |> Seq.max
+    printfn "X: %A Y: %A Z: %A" (xs,xe) (ys,ye) (zs,ze) 
+    0
 
 sw.Restart()
 let r2 = solve2 "Day22_sample3.txt"
