19fc1f631194d817e3d7ecd7befd2e8fe78103be
Julien Adam <julien.adam@gmail.com>
2021-12-24 16:10:22 +0100

    Day22 part1 done, part2 in naïve mode isn't going to cut it

--- Puzzles/2021/Day22.fsx
+++ Puzzles/2021/Day22.fsx
@@ -23,4 +23,60 @@ let mapLine line =
     let (instruction:RebootInstruction) = (xRange, yRange, zRange), active
     instruction
 
-mapLine "off x=-54112..-39298,y=-85059..-49293,z=-27449..7877"
+// mapLine "off x=-54112..-39298,y=-85059..-49293,z=-27449..7877"
+
+let enumRange (s,e) = seq { 
+    for i = s to e do yield i
+}
+
+let solve1 fileName = 
+    let input = getInputPath fileName |> File.ReadAllLines |> Seq.map mapLine
+    
+    let filterInitializationCubes (instructions:RebootInstruction seq) =
+        let inline insideInitBounds (start, endp) = start >= -50 && endp <= 50
+        instructions |> Seq.filter (fun ((xr, yr, zr), _) ->
+            insideInitBounds xr && insideInitBounds yr && insideInitBounds zr
+        )
+
+    let filteredInput = input |> filterInitializationCubes |> Seq.toList
+
+    let grid = new Dictionary<(int * int * int), bool>()
+    
+    filteredInput 
+    |> Seq.iter(fun ((xr,yr,zr), state) -> 
+        for x in enumRange xr do
+            for y in enumRange yr do
+                for z in enumRange zr do
+                    grid.[(x,y,z)] <- state
+    )
+
+    grid.Values |> Seq.filter id |> Seq.length
+
+
+assert(solve1 "Day22_Sample1.txt" = 39)
+assert(solve1 "Day22_Sample2.txt" = 590784)
+let sw = Stopwatch.StartNew()
+let result = solve1 "Day22.txt"
+printfn "Day 22 Part 1 solution : %i. Took %A" result sw.Elapsed
+
+
+let solve2 fileName = 
+    let input = getInputPath fileName |> File.ReadAllLines |> Seq.map mapLine |> Seq.toList
+    let totalInstructions = input.Length
+    let grid = new Dictionary<(int * int * int), bool>()
+    
+    input
+    |> Seq.iteri(fun i ((xr,yr,zr), state) -> 
+        printfn "Iteration %i of %i" i totalInstructions
+        for x in enumRange xr do
+            for y in enumRange yr do
+                for z in enumRange zr do
+                    grid.[(x,y,z)] <- state
+    )
+
+    grid.Values |> Seq.filter id |> Seq.length
+
+sw.Restart()
+let r2 = solve2 "Day22_sample3.txt"
+printfn "Day 22 Part 2 sample  : %i. Took %A" r2 sw.Elapsed
+// TODO : filter again for init cubes after reboot proc. Shouldn't be necessary as the coords don't change
