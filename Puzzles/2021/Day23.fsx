7766a7cd5bd28002433066e0ba91a5b0c0009835
Julien Adam <julien.adam@gmail.com>
2021-12-25 21:58:55 +0100

    Parsing for day 23

---
+++ Puzzles/2021/Day23.fsx
@@ -0,0 +1,16 @@
+﻿#load "../../Tools.fsx"
+
+open System
+open System.Collections.Generic
+open System.Diagnostics
+open System.IO
+open System.Text.RegularExpressions
+open Tools
+
+let lines = getInputPath "Day23_sample1.txt" |> File.ReadAllLines 
+let roomA = lines.[2].[3], lines.[3].[3]
+let roomB = lines.[2].[5], lines.[3].[5]
+let roomC = lines.[2].[7], lines.[3].[7]
+let roomD = lines.[2].[9], lines.[3].[9]
+
+printfn "%A %A %A %A" roomA roomB roomC roomD
