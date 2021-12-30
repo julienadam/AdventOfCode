043f801364d4604d6a3dbc35a97814de8aa40307
Julien Adam <julien.adam@gmail.com>
2021-12-30 23:09:41 +0100

    Day 24. Read input as equations, implemented basic reduction
    Need work to prune some conditions that are obvs. not satisfiable
    like (i1 % 26) + 13 = i2

--- Puzzles/2021/Day24.fsx
+++ Puzzles/2021/Day24.fsx
@@ -49,155 +49,251 @@ let mapLine (l:string) =
     
 let mapAluInstructions input = input |> Seq.map mapLine |> Seq.toList
 
-type ALU = int64 -> Inst list -> int[]
+// type ALU = int64 -> Inst list -> int[]
 
-module AluInterpreter =
-
-    let rec aluInterpreterRec (input:int64) (divisor:int64) instructions (vars:int[]) =
-        let inline getOperandValue (vars: int[]) =
-            function
-            | Variable v2 -> vars.[v2 |> int]
-            | Const x -> x
-
-        let inline apply (v1:Var) operation operand vars = 
-            let v1Int = v1 |> int
-            let b = getOperandValue vars operand
-            let a = vars.[v1Int]
-            vars.[v1Int] <- operation a b
-            vars
-
-        // Special case as we need to check the operands
-        let inline applyMod (v1:Var) operand (vars: int[]) = 
-            let v1Int = v1 |> int
-            let a = vars.[v1Int]
-            if a < 0 then
-                failwithf "Left operand for %% op is negative: %i" a
-            let b = getOperandValue vars operand
-            if b <= 0 then
-                failwithf "Right operand for %% op is <=0 : %i" b
-
-            vars.[v1Int] <- a % b
-            vars
-
-        let inline eql a b = if a = b then 1 else 0
-
-        match instructions with
-        | [] -> vars
-        | inst :: instrTail ->
-            match inst with
-            | Inp v -> 
-                if divisor = 0L then
-                    failwithf "Divisor is 0, happens if we read too many inputs"
-                Array.set vars (v |> int) ((input / divisor) |> int)
-                aluInterpreterRec (input % divisor) (divisor / 10L) instrTail vars 
-            | Add (v1, op) -> aluInterpreterRec input divisor instrTail (vars |> apply v1 (+) op )
-            | Mul (v1, op) -> aluInterpreterRec input divisor instrTail (vars |> apply v1 (*) op )
-            | Div (v1, op) -> aluInterpreterRec input divisor instrTail (vars |> apply v1 (/) op )
-            | Mod (v1, op) -> aluInterpreterRec input divisor instrTail (vars |> applyMod v1 op )
-            | Eql (v1, op) -> aluInterpreterRec input divisor instrTail (vars |> apply v1 eql op )
-
-    let aluInterpreter input instructions = 
-        let div = if input.ToString().Length = 1 then 1L else 10L*((input.ToString().Length |> int64) - 1L)
-        aluInterpreterRec input div instructions [|0;0;0;0|]
-
-module Tests = 
-
-    let testMod alu =
-        let modInstr = ["inp x"; "mod x 2" ] |> mapAluInstructions
-        assert(alu 4L modInstr = [|0;0;0;0|])
-        assert(alu 3L modInstr = [|0;1;0;0|])
+type Operator = 
+    | Plus
+    | Multiply
+    | Divide
+    | Modulo
     
-        try
-            let modOnNegA = ["inp x"; "add x -1"; "mod x 2" ] |> mapAluInstructions
-            alu 0L modOnNegA |> ignore
-            assert(false)
-        with _ -> ()
+type Equation = 
+    | Constant of int
+    | Input of string
+    | Operation of Equation * Operator * Equation
+
+let opStr = function | Plus -> "+" | Multiply -> "*" | Divide -> "/" | Modulo -> "%"
+
+let rec printEquation eq =
+    match eq with
+    | Constant x -> sprintf "%i" x
+    | Input i -> sprintf "%s" i
+    | Operation (eq1, op, eq2) ->
+        sprintf "(%s %s %s)" (printEquation eq1) (opStr op) (printEquation eq2)
+
+type Condition = 
+    | AreEqual of Equation * Equation
+    | AreDifferent of Equation * Equation
+
+let printCondition = 
+    function
+    | AreEqual (e1,e2) -> sprintf "(%s = %s)" (printEquation e1) (printEquation e2)
+    | AreDifferent (e1,e2) -> sprintf "(%s <> %s)" (printEquation e1) (printEquation e2)
     
-        try
-            let modOnNegB = ["inp x"; "mod x -2" ] |> mapAluInstructions
-            alu 2L modOnNegB |> ignore
-            assert(false)
-        with _ -> ()
-
-        try
-            let modOnZeroB = ["inp x"; "mod x 0" ] |> mapAluInstructions
-            alu 2L modOnZeroB |> ignore
-            assert(false)
-        with _ -> ()
-
-        
-    let test3Times alu = 
-        
-        (*
-        Here is an ALU program which takes two input numbers, then sets z to 1 if the second input 
-        number is three times larger than the first input number, or sets z to 0 otherwise:
-        *)
-        let alu3Instrs = ["inp z"; "inp x"; "mul z 3"; "eql z x" ] |> mapAluInstructions
-        assert(alu 13L alu3Instrs = [|0; 3; 0; 1|])
-        assert(alu 14L alu3Instrs = [|0; 4; 0; 0|])
-        //assert(alu [-2; -6] alu3Instrs = [|0; -6; 0; 1|])
+let apply index instr ((expansion, condition):Equation[] * Condition list) =
+    let getOperandValue =
+        function
+        | Variable v -> expansion.[v |> int]
+        | Const x -> Constant x
     
-    let testDecompBits alu = 
-        (*
-        Here is an ALU program which takes a non-negative integer as input, converts it into binary, 
-        and stores the lowest (1's) bit in z, the second-lowest (2's) bit in y, the third-lowest (4's) 
-        bit in x, and the fourth-lowest (8's) bit in w:
-        *)
-        let aluDecompBits = ["inp w"; "add z w"; "mod z 2"; "div w 2"; "add y w"; "mod y 2"; "div w 2"; "add x w"; "mod x 2"; "div w 2"; "mod w 2"] |> mapAluInstructions
-        assert(alu 9L aluDecompBits = [|1; 0; 0; 1|])
-        assert(alu 0L aluDecompBits = [|0; 0; 0; 0|])
-        assert(alu 1L aluDecompBits = [|0; 0; 0; 1|])
-        assert(alu 2L aluDecompBits = [|0; 0; 1; 0|])
-        assert(alu 4L aluDecompBits = [|0; 1; 0; 0|])
-        assert(alu 8L aluDecompBits = [|1; 0; 0; 0|])
-    
-Tests.testMod AluInterpreter.aluInterpreter
-Tests.test3Times AluInterpreter.aluInterpreter
-Tests.test3Times AluInterpreter.aluInterpreter
-Tests.testDecompBits AluInterpreter.aluInterpreter
-    
-// Well, this isn't going to work, at all. Assuming a 4GHz CPU, 99.999.999.999.999 number to check would mean
-// So, about 10 months if the whole check could be done in a single CPU cycle, which it definitely is NOT.
-(99_999_999_999_999L / 4_000_000L) / 60L / 60L / 24L // 289 days
-// The first digit can be ignored, since it has no bearing on the output (for my input) so in fact that would be :
-(9_999_999_999_999L / 4_000_000L) / 60L / 60L / 24L // 28 days
-
-getInputPath "day24.txt" |> File.ReadAllLines |> mapAluInstructions
-
-let aluInstructions = getInputPath "day24.txt" |> File.ReadAllLines |> mapAluInstructions
-
-let isValidSerialNumber (alu:ALU) input =
-    try
-        let result = alu input aluInstructions
-        if result.[0] <> 0 && result.[1] <> 0 && result.[2] <> 0 && result.[2] <> 0 then
-            Some input
+    match instr with
+    | Inp v ->
+        let vInt = v |> int
+        expansion.[vInt] <- sprintf "i%i" index |> Input
+        [expansion, condition]
+    | Add (v,a) ->
+        let vInt = v |> int
+        let aVal = getOperandValue a
+        match expansion.[vInt], aVal with
+        | Constant 0, _ -> // 0 + b = b
+            expansion.[vInt] <- aVal
+        | _, Constant 0 -> // a + 0 = a
+            ()
+        | Constant a, Constant b ->
+            expansion.[vInt] <- Constant (a + b)
+        | _ -> 
+            expansion.[vInt] <- Operation (expansion.[vInt], Plus, aVal)
+        [expansion, condition]
+    | Mul (v,a) ->
+        let vInt = v |> int
+        let aVal = getOperandValue a
+        if aVal = Constant 0 || expansion.[vInt] = Constant 0 then
+            expansion.[vInt] <- Constant 0
+        else if aVal <> Constant 1 then
+            expansion.[vInt] <- Operation (expansion.[vInt], Multiply, aVal)
+        [expansion, condition]
+    | Mod (v,a) ->
+        let vInt = v |> int
+        if expansion.[vInt] = Constant 0 then // 0 mod x = 0
+            [expansion, condition]
         else
-            None
-    with _ ->
-        None
+            let aVal = getOperandValue a
+            // Check rightside >= 0 and leftside > 0 ?
+            expansion.[vInt] <- Operation (expansion.[vInt], Modulo, aVal)
+            [expansion, condition]
+    | Div (v,a) ->
+        let vInt = v |> int
+        let aVal = getOperandValue a
+        if aVal = Constant 0 then
+            failwithf "Division by zero"
+        else if aVal <> Constant 1 then // div by one is noop
+            expansion.[vInt] <- Operation (expansion.[vInt], Divide, aVal)
+        [expansion, condition]
+    | Eql (v,a) ->
+        let vInt = v |> int
+        let aVal = getOperandValue a
+        // TODO: Expand the conditions by implementing a isSatisfiable cond function
+        match expansion.[vInt], aVal with
+        | eq1, eq2 when eq1 = eq2 -> 
+            expansion.[vInt] <- Constant 1
+            [expansion, condition]
+        | Constant a, Constant b when a = b ->
+            expansion.[vInt] <- Constant 1
+            [expansion, condition]
+        | Constant a, Constant b when a <> b ->
+            expansion.[vInt] <- Constant 0
+            [expansion, condition]
+        | Constant a, Input _ when a >= 10 || a < 0 ->
+            expansion.[vInt] <- Constant 0
+            [expansion, condition]
+        | _ ->
+            let e1 = expansion |> Array.copy
+            let e2 = expansion |> Array.copy
+            e1.[vInt] <- Constant 1
+            e1.[vInt] <- Constant 0
+            [
+                (e1, (AreEqual(expansion.[vInt], aVal)) :: condition); 
+                (e2, (AreDifferent(expansion.[vInt], aVal)) :: condition)]
 
-let genSerialNumbers (upperBound:int64) = seq {
-    let mutable i = upperBound
-    while i > 0 do
-        yield i
-        //if i % 100000L = 0L then
-        //    printfn "%i" i
-        i <- i - 1L
-}
+let rec expand index instructions expansions =
+    match instructions with
+    | [] -> expansions
+    | instr::instrTail ->
+        printfn "instruction : %A. Expansions : %i. Index %i" instr (expansions |> List.length) index
+        let nextExpansions = expansions |> List.collect (fun e -> apply index instr e)
+        let nextI = match instr with | Inp _ -> index + 1 | _ -> index
+        expand nextI instrTail nextExpansions
 
-genSerialNumbers 99_999_999_999_999L |> Seq.last
+let initialState () = [[|Constant 0; Constant 0; Constant 0; Constant 0|], []]
 
-let solve1 alu max = 
-    let result = genSerialNumbers max |> Seq.tryPick (isValidSerialNumber alu)
-    printfn "%A" result
+let aluDecompBits = ["inp w"; "add z w"; "mod z 2"; "div w 2"; "add y w"; "mod y 2"; "div w 2"; "add x w"; "mod x 2"; "div w 2"; "mod w 2"] |> mapAluInstructions
+expand 1 aluDecompBits (initialState())
 
-solve1 AluInterpreter.aluInterpreter 999999L
+let day24Input = getInputPath "Day24.txt" |> File.ReadAllLines |> mapAluInstructions |> List.take 50
+let expanded = expand 1 day24Input (initialState())
+expanded |> Seq.iter (fun (eq, cond) -> 
+    printf "w=%s; " (printEquation eq.[0])
+    printf "x=%s; " (printEquation eq.[1])
+    printf "y=%s; " (printEquation eq.[2])
+    printf "z=%s; " (printEquation eq.[3])
+    match cond with
+    | [] -> ()
+    | [a] -> printf "if %s" (printCondition a)
+    | head::tail -> 
+        printf "if %s" (printCondition head)
+        tail |> Seq.iter (fun c -> printf " && %s" (printCondition c))
+    printfn ""
+    )
+//printEquation 
 
-// TODO Transform instructions into a compiled F# expression ? Should be orders of mag faster ?
+//module AluInterpreter =
 
-let i1 = 9
-let i2 = 9
-let i3 = 9
-let i4 = 9
-(((((i1 + 8) * 26 + i2 + 8) % 26) + 13 + i3 + 3) * 26 + i4 + 10) % 26
-i4 + 10
+//    let rec aluInterpreterRec (input:int64) (divisor:int64) instructions (vars:int[]) =
+//        let inline getOperandValue (vars: int[]) =
+//            function
+//            | Variable v2 -> vars.[v2 |> int]
+//            | Const x -> x
+
+//        let inline apply (v1:Var) operation operand vars = 
+//            let v1Int = v1 |> int
+//            let b = getOperandValue vars operand
+//            let a = vars.[v1Int]
+//            vars.[v1Int] <- operation a b
+//            vars
+
+//        // Special case as we need to check the operands
+//        let inline applyMod (v1:Var) operand (vars: int[]) = 
+//            let v1Int = v1 |> int
+//            let a = vars.[v1Int]
+//            if a < 0 then
+//                failwithf "Left operand for %% op is negative: %i" a
+//            let b = getOperandValue vars operand
+//            if b <= 0 then
+//                failwithf "Right operand for %% op is <=0 : %i" b
+
+//            vars.[v1Int] <- a % b
+//            vars
+
+//        let inline eql a b = if a = b then 1 else 0
+
+//        match instructions with
+//        | [] -> vars
+//        | inst :: instrTail ->
+//            match inst with
+//            | Inp v -> 
+//                if divisor = 0L then
+//                    failwithf "Divisor is 0, happens if we read too many inputs"
+//                Array.set vars (v |> int) ((input / divisor) |> int)
+//                aluInterpreterRec (input % divisor) (divisor / 10L) instrTail vars 
+//            | Add (v1, op) -> aluInterpreterRec input divisor instrTail (vars |> apply v1 (+) op )
+//            | Mul (v1, op) -> aluInterpreterRec input divisor instrTail (vars |> apply v1 (*) op )
+//            | Div (v1, op) -> aluInterpreterRec input divisor instrTail (vars |> apply v1 (/) op )
+//            | Mod (v1, op) -> aluInterpreterRec input divisor instrTail (vars |> applyMod v1 op )
+//            | Eql (v1, op) -> aluInterpreterRec input divisor instrTail (vars |> apply v1 eql op )
+
+//    let aluInterpreter input instructions = 
+//        let div = if input.ToString().Length = 1 then 1L else 10L*((input.ToString().Length |> int64) - 1L)
+//        aluInterpreterRec input div instructions [|0;0;0;0|]
+
+//module Tests = 
+
+//    let testMod alu =
+//        let modInstr = ["inp x"; "mod x 2" ] |> mapAluInstructions
+//        assert(alu 4L modInstr = [|0;0;0;0|])
+//        assert(alu 3L modInstr = [|0;1;0;0|])
+    
+//        try
+//            let modOnNegA = ["inp x"; "add x -1"; "mod x 2" ] |> mapAluInstructions
+//            alu 0L modOnNegA |> ignore
+//            assert(false)
+//        with _ -> ()
+    
+//        try
+//            let modOnNegB = ["inp x"; "mod x -2" ] |> mapAluInstructions
+//            alu 2L modOnNegB |> ignore
+//            assert(false)
+//        with _ -> ()
+
+//        try
+//            let modOnZeroB = ["inp x"; "mod x 0" ] |> mapAluInstructions
+//            alu 2L modOnZeroB |> ignore
+//            assert(false)
+//        with _ -> ()
+
+        
+//    let test3Times alu = 
+        
+//        (*
+//        Here is an ALU program which takes two input numbers, then sets z to 1 if the second input 
+//        number is three times larger than the first input number, or sets z to 0 otherwise:
+//        *)
+//        let alu3Instrs = ["inp z"; "inp x"; "mul z 3"; "eql z x" ] |> mapAluInstructions
+//        assert(alu 13L alu3Instrs = [|0; 3; 0; 1|])
+//        assert(alu 14L alu3Instrs = [|0; 4; 0; 0|])
+//        //assert(alu [-2; -6] alu3Instrs = [|0; -6; 0; 1|])
+    
+//    let testDecompBits alu = 
+//        (*
+//        Here is an ALU program which takes a non-negative integer as input, converts it into binary, 
+//        and stores the lowest (1's) bit in z, the second-lowest (2's) bit in y, the third-lowest (4's) 
+//        bit in x, and the fourth-lowest (8's) bit in w:
+//        *)
+//        let aluDecompBits = ["inp w"; "add z w"; "mod z 2"; "div w 2"; "add y w"; "mod y 2"; "div w 2"; "add x w"; "mod x 2"; "div w 2"; "mod w 2"] |> mapAluInstructions
+//        assert(alu 9L aluDecompBits = [|1; 0; 0; 1|])
+//        assert(alu 0L aluDecompBits = [|0; 0; 0; 0|])
+//        assert(alu 1L aluDecompBits = [|0; 0; 0; 1|])
+//        assert(alu 2L aluDecompBits = [|0; 0; 1; 0|])
+//        assert(alu 4L aluDecompBits = [|0; 1; 0; 0|])
+//        assert(alu 8L aluDecompBits = [|1; 0; 0; 0|])
+    
+//Tests.testMod AluInterpreter.aluInterpreter
+//Tests.test3Times AluInterpreter.aluInterpreter
+//Tests.test3Times AluInterpreter.aluInterpreter
+//Tests.testDecompBits AluInterpreter.aluInterpreter
+    
+//// Well, this isn't going to work, at all. Assuming a 4GHz CPU, 99.999.999.999.999 number to check would mean
+//// So, about 10 months if the whole check could be done in a single CPU cycle, which it definitely is NOT.
+//(99_999_999_999_999L / 4_000_000L) / 60L / 60L / 24L // 289 days
+//// The first digit can be ignored, since it has no bearing on the output (for my input) so in fact that would be :
+//(9_999_999_999_999L / 4_000_000L) / 60L / 60L / 24L // 28 days
+
