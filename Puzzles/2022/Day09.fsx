7ff30255a98ae2e46579b59801aaf3e5b33131db
Julien Adam <julien.adam@gmail.com>
2022-12-17 19:15:19 +0100

    Day 09 done
    F# FTW

--- Puzzles/2022/Day09.fsx
+++ Puzzles/2022/Day09.fsx
@@ -2,20 +2,14 @@
 
 open System
 open System.IO
+open System.Collections.Generic
 open Tools
-
-// type Instruction = {
-//   Direction : Compass
-//   Length : 
-// }
 
 let mapLine (dir : string, len) = 
   let c = match dir.[0] with | 'R' -> Compass.Right | 'L' -> Compass.Left | 'U' -> Compass.Up | 'D' -> Compass.Down
   c, len |> Int32.Parse
 
 let getInput p = File.ReadAllLines(getInputPath2022 p) |> Seq.map (split2 ' ') |> Seq.map mapLine
-
-let i = getInput "Day09.txt" |> Seq.toList
 
 type HeadPosition =
 | Cover
@@ -28,32 +22,70 @@ type HeadPosition =
 | DownLeft
 | DownRight
 
-let initialState = (0, 0), Cover
+type coords = int * int
 
-//let newHeadPos (dir:Compass) (x,y) =
-//    match dir with
-//    | Compass.Right -> (x+1,y)
-//    | Compass.Left -> (x-1,y)
-//    | Compass.Up -> (x,y-1)
-//    | Compass.Down -> (x,y+1)
-
-let makeMove (headDir:Compass) ((tailX,tailY), headPos) placesVisited =
+let makeMove (headDir:Compass) (headPos, (tailX,tailY)) (placesVisited:HashSet<coords>) =
+    let unchanged = tailX, tailY
+    let newHeadPos, newTailCoords =
+        match headPos, headDir with 
+        | Cover,    Compass.Right   -> Right,       unchanged
+        | Cover,    Compass.Left    -> Left,        unchanged
+        | Cover,    Compass.Up      -> Up,          unchanged
+        | Cover,    Compass.Down    -> Down,        unchanged
+        | Right,    Compass.Right   -> Right,       (tailX+1, tailY)
+        | Right,    Compass.Left    -> Cover,       unchanged
+        | Right,    Compass.Down    -> DownRight,   unchanged
+        | Right,    Compass.Up      -> UpRight,     unchanged
+        | Left,     Compass.Left    -> Left,        (tailX-1, tailY)
+        | Left,     Compass.Right   -> Cover,       unchanged
+        | Left,     Compass.Down    -> DownLeft,    unchanged
+        | Left,     Compass.Up      -> UpLeft,      (tailX, tailY)
+        | Up,       Compass.Up      -> Up,          (tailX, tailY-1)
+        | Up,       Compass.Down    -> Cover,       unchanged
+        | Up,       Compass.Right   -> UpRight,     unchanged
+        | Up,       Compass.Left    -> UpLeft,      unchanged
+        | Down,     Compass.Down    -> Down,        (tailX, tailY+1)
+        | Down,     Compass.Up      -> Cover,       unchanged
+        | Down,     Compass.Right   -> DownRight,   unchanged
+        | Down,     Compass.Left    -> DownLeft,    unchanged
+        | UpRight,  Compass.Down    -> Right,       unchanged
+        | UpRight,  Compass.Up      -> Up,          (tailX + 1, tailY - 1)
+        | UpRight,  Compass.Right   -> Right,       (tailX + 1, tailY - 1)
+        | UpRight,  Compass.Left    -> Up,          unchanged
+        | UpLeft,   Compass.Down    -> Left,        unchanged
+        | UpLeft,   Compass.Up      -> Up,          (tailX - 1, tailY - 1)
+        | UpLeft,   Compass.Right   -> Up,          unchanged
+        | UpLeft,   Compass.Left    -> Left,        (tailX - 1, tailY - 1)
+        | DownRight,Compass.Down    -> Down,        (tailX + 1, tailY + 1)
+        | DownRight,Compass.Up      -> Right,       unchanged
+        | DownRight,Compass.Left    -> Down,        unchanged
+        | DownRight,Compass.Right   -> Right,       (tailX + 1, tailY + 1)
+        | DownLeft, Compass.Down    -> Down,        (tailX - 1, tailY + 1)
+        | DownLeft, Compass.Up      -> Left,        unchanged
+        | DownLeft, Compass.Left    -> Left,        (tailX - 1, tailY + 1)
+        | DownLeft, Compass.Right   -> Down,        unchanged
     
-    match headPos, headDir with 
-    | Cover, Compass.Right -> Right, (tailX,tailY)
-    | Cover, Compass.Left -> Left, (tailX,tailY)
-    | Cover, Compass.Up -> Up, (tailX,tailY)
-    | Cover, Compass.Down -> Down, (tailX,tailY)
-    | Right, Compass.Right -> Right, (tailX+1,tailY)
-    | Right, Compass.Left -> Cover, (tailX-1,tailY)
-    | Right, Compass.Down -> UpLeft, (tailX,tailY)
-    | Right, Compass.Up -> DownLeft, (tailX,tailY)
-    | Left, Compass.Left -> Left, (tailX-1,tailY)
-    | Left, Compass.Right-> Cover, (tailX+1,tailY)
-    | Left, Compass.Down -> UpRight, (tailX,tailY)
-    | Left, Compass.Up -> DownRight, (tailX,tailY)
-    | Up, Compass.Up -> Up, (tailX,tailY-1)
-    | Up, Compass.Down -> Cover, (tailX,tailY)
-    | Up, Compass.Right -> DownLeft, (tailX,tailY)
-    | Up, Compass.Left -> DownRight, (tailX,tailY)
-    | Down, Compass.Down -> Cover, (tailX,tailY+1)
+    if not (newTailCoords = unchanged) then
+        placesVisited.Add(newTailCoords) |> ignore
+    
+    newHeadPos, newTailCoords
+
+let solve1 input =
+    let instructions = getInput input |> Seq.toList
+    let visited = new HashSet<coords>([(0,0)])
+    let initialState = Cover, (0, 0)
+
+    let finalState = 
+        instructions |> Seq.fold (fun state (dir, len) ->
+            [1..len] |> Seq.fold (fun s _ -> 
+                makeMove dir s visited
+            ) state
+        ) initialState
+
+    // finalState |> Dump
+
+    visited.Count
+
+    
+solve1 "Day09.txt" |> Dump
+
