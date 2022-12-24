9f35943b4a8e4fb9936d9e79d94aaac58935e912
Julien Adam <julien.adam@gmail.com>
2022-12-24 11:47:06 +0100

    Day 10 part 1 done

--- Puzzles/2022/Day10.fsx
+++ Puzzles/2022/Day10.fsx
@@ -15,4 +15,35 @@ let mapLine line =
 
 let getInput p = File.ReadAllLines(getInputPath2022 p) |> Seq.map mapLine
 
-getInput "Day10.txt" |> Dump
+// getInput "Day10_sample1.txt" |> Dump
+
+let execute (instructions: Instruction seq) = seq {
+    let mutable cycle = 1
+    let mutable x = 1
+    yield cycle, x
+    for inst in instructions do
+        match inst with
+        | Noop -> 
+            cycle <- cycle + 1
+            yield cycle, x
+        | Addvx value -> 
+            cycle <- cycle + 1
+            yield cycle, x
+            cycle <- cycle + 1
+            yield cycle, x
+            x <- x + value
+}
+
+let solve1 (instructions: Instruction seq) =
+    let cycles = 
+        execute instructions 
+        |> Seq.toArray
+        //|> Array.map (fun (c,x) -> 
+        //    printfn "%i\t%i" c x
+        //    c,x)
+    
+    [20; 60; 100; 140; 180; 220] |> Seq.map (fun i -> i * (cycles[i] |> snd)) |> Seq.sum
+
+getInput "Day10.txt"
+|> solve1
+|> Dump
