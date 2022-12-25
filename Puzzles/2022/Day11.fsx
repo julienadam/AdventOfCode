de2bede7161edf7e6158701512eebfda19154cb7
Julien Adam <julien.adam@gmail.com>
2022-12-25 10:16:25 +0100

    Day 11 part2 done
    Tried bigint and failed
    Learned about `open Checked`, very nice
    Then had the idea about Least Common Denominator since all the checks
    are modulo ops.

--- Puzzles/2022/Day11.fsx
+++ Puzzles/2022/Day11.fsx
@@ -2,21 +2,22 @@
 
 open System
 open System.IO
+open Checked // Replaces normal, unchecked arithmetic ops with checked ones. Clever !
 open Tools
 
 type Op =
-    | Add of int
-    | Multiply of int
+    | Add of int64
+    | Multiply of int64
     | Square
 
 type Monkey = {
     id : int
-    mutable items : int list
+    mutable items : int64 list
     op : Op
-    test : int
+    test : int64
     onTrue : int
     onFalse : int
-    mutable itemsInspected : int
+    mutable itemsInspected : int64
 }
 
 let mapOp (line:string) =
@@ -31,10 +32,10 @@ let mapMonkey (lines: string array) =
     let monkeyId = Int32.Parse(lines.[0].[7] |> string)
     let items = 
         lines.[1].Substring(18).Split([|", "|], StringSplitOptions.None)
-        |> Seq.map Int32.Parse
+        |> Seq.map Int64.Parse
         |> Seq.toList
     let op = mapOp lines.[2]
-    let test = Int32.Parse(lines.[3].Substring(21))
+    let test = Int64.Parse(lines.[3].Substring(21))
     let targetIfTrue = Int32.Parse(lines.[4].Substring(29))
     let targetIfFalse = Int32.Parse(lines.[5].Substring(30))
     { id = monkeyId; items = items; test = test; op = op; onTrue = targetIfTrue; onFalse = targetIfFalse; itemsInspected = 0 }
@@ -45,21 +46,32 @@ let getInput p =
     |> Seq.map mapMonkey
     |> Seq.toArray
 
-let updateWorry i op =
-    match op with
-    | Square -> i * i
-    | Add j -> i + j
+let updateWorry i =
+    function
+    | Square -> (i * i)
+    | Add j -> (i + j)
     | Multiply j -> i * j
 
-let round i (monkeys:Monkey array) =
+
+let updateWorryLcm lcm i =
+    function
+    | Square -> (i * i) % lcm
+    | Add j -> (i + j) % lcm
+    | Multiply j -> i * j % lcm
+
+
+let round i (lcm: int64 option) (monkeys:Monkey array) =
     monkeys |> Array.iter (fun monkey ->
         monkey.items |> Seq.iter (fun item ->
-            monkey.itemsInspected <- monkey.itemsInspected + 1
-            let worry = updateWorry item monkey.op
-            let reducedWorry = Math.Round((worry |> double) / 3.0) |> int
-            printfn "Worry for %i was updated to %i and rounded to %i" item worry reducedWorry
+            monkey.itemsInspected <- monkey.itemsInspected + 1L
+            let reducedWorry = 
+                if lcm.IsNone then
+                    let worry = updateWorry item monkey.op
+                    Math.Floor((worry |> double) / 3.0) |> int64
+                else 
+                    updateWorryLcm lcm.Value item monkey.op
             let target = 
-                if reducedWorry % monkey.test = 0 then
+                if reducedWorry % monkey.test = 0L then
                     monkey.onTrue
                 else 
                     monkey.onFalse
@@ -67,25 +79,46 @@ let round i (monkeys:Monkey array) =
         )
         monkey.items <- []
     )
-    printfn ""
-    printfn "After round %i" i
-    monkeys |> Dump
+    if i = 1 || i = 20 || i % 1000 = 0 then
+        printfn "== After round %i ==" i
+        monkeys |> Seq.iter (fun m -> 
+            printfn "Monkey %i inspected items %i times" m.id m.itemsInspected
+        )
 
 
 let solve1 monkeys =
-    [1..1] 
-    |> Seq.iter (fun i -> round i monkeys |> ignore)
+    [1..20] 
+    |> Seq.iter (fun i -> round i None monkeys |> ignore)
     
     let mostActive = 
         monkeys 
-        |> Array.map(fun m -> m.itemsInspected) 
+        |> Array.map(fun m -> m.itemsInspected)
+        |> Array.sortDescending
+        |> Array.take 2
+        |> Dump
+
+    mostActive.[0] * mostActive.[1]
+    
+//getInput "Day11_sample1.txt"
+//|> solve1
+//|> Dump
+
+let solve2 monkeys =
+    let lcm = monkeys |> Seq.fold (fun state m -> state * m.test) 1L
+    printfn "Least common denominator is %i" lcm
+
+    [1..10000] 
+    |> Seq.iter (fun i -> round i (Some lcm) monkeys |> ignore)
+    
+    let mostActive =
+        monkeys
+        |> Array.map(fun m -> m.itemsInspected)
         |> Array.sortDescending 
         |> Array.take 2
         |> Dump
 
     mostActive.[0] * mostActive.[1]
 
-getInput "Day11_sample1.txt"
-|> solve1
+getInput "Day11.txt"
+|> solve2
 |> Dump
-
