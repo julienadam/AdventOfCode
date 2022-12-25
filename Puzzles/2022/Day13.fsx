9866d0299e5d6c1f13760bd021e549b112448a85
Julien Adam <julien.adam@gmail.com>
2022-12-25 21:10:59 +0100

    Day 13 part 2 done

--- Puzzles/2022/Day13.fsx
+++ Puzzles/2022/Day13.fsx
@@ -18,10 +18,10 @@ let rec compare (left:FSharp.Data.JsonValue) (right:FSharp.Data.JsonValue) =
     match left, right with
     | FSharp.Data.JsonValue.Number l, FSharp.Data.JsonValue.Number r ->
         if l < r then
-            printfn "True because %A < %A" l r
+            //printfn "True because %A < %A" l r
             Some true
         else if l > r then
-            printfn "false because %A > %A" l r
+            //printfn "false because %A > %A" l r
             Some false
         else
             None
@@ -33,10 +33,10 @@ let rec compare (left:FSharp.Data.JsonValue) (right:FSharp.Data.JsonValue) =
         | Some b -> Some b
         | None ->
             if leftItems.Length < rightItems.Length then
-                printfn "True because %A is smaller than %A" leftItems rightItems
+                //printfn "True because %A is smaller than %A" leftItems rightItems
                 Some true
             else if leftItems.Length > rightItems.Length then
-                printfn "False because %A is bigger than %A" leftItems rightItems
+                //printfn "False because %A is bigger than %A" leftItems rightItems
                 Some false
             else
                 None
@@ -55,8 +55,42 @@ let solve1 pairs =
     |> Seq.map fst
     |> Seq.sum
 
-let input = getInput "Day13_sample1.txt"
+//let input = getInput "Day13_sample1.txt"
+//input
+//|> solve1
+//|> Dump
 
-input 
-|> solve1 
-|> Dump
+let dividerPacket2 = parseData "[[2]]"
+let dividerPacket6 = parseData "[[6]]"
+
+let getInput2 p = 
+    File.ReadAllText(getInputPath2022 p)
+        .Replace("\r\n\r\n", "\r\n")
+        .Split([|"\r\n"|], StringSplitOptions.RemoveEmptyEntries)
+    |> Seq.map parseData
+    |> Seq.append [dividerPacket2; dividerPacket6]
+    |> Seq.toArray
+
+let input2 = getInput2 "Day13_sample1.txt" |> Dump
+
+
+let solve2 input = 
+    let compareBound l r = 
+        match compare l r with
+        | Some b -> if b then -1 else 1
+        | None -> 0
+
+    let sorted = 
+        input
+        |> Seq.sortWith compareBound
+        |> Seq.toArray
+
+    sorted |> Seq.iter (fun a -> 
+        printfn "%s" (a.ToString(FSharp.Data.JsonSaveOptions.DisableFormatting)))
+
+    let i1 = (Array.IndexOf(sorted, dividerPacket2) + 1)
+    let i2 = (Array.IndexOf(sorted, dividerPacket6) + 1)
+    i1 * i2
+// |> Dump
+
+input2 |> solve2
