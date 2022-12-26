f7e5d6f4883a9d15779d95cbfdf79c4e0cf69835
Julien Adam <julien.adam@gmail.com>
2022-12-26 11:30:18 +0100

    Day15 part1

--- Puzzles/2022/Day15.fsx
+++ Puzzles/2022/Day15.fsx
@@ -12,10 +12,48 @@ let mapSensor line =
         failwithf "Could not parse line %s" line
     else
         ((mInt "sx" m), (mInt "sy" m)), ((mInt "bx" m), (mInt "by" m)) 
-    
+
+type SensorArea = {
+    sensor : int * int
+    beacon : int * int
+    range : int
+}
+
 let getInput p = 
     File.ReadAllLines(getInputPath2022 p) 
     |> Seq.map mapSensor
+    |> Seq.map (fun ((sx,sy),(bx,by)) -> 
+        { sensor = sx, sy; beacon = bx, by; range = manhattanDistance sx sy bx by })
     |> Seq.toArray
 
 getInput "Day15_sample1.txt" |> Dump
+
+let isInsideSensorRange point sensor range =
+    // A point is inside the coverage area if its distance is < beacon range
+    manhattanDistPoints point sensor <= range
+
+let solve1 lineY sensorAreas =
+    // find the limits of the sensor range on the X axis, left and right
+    let minX = sensorAreas |> Seq.map (fun s -> (s.sensor |> fst) - s.range) |> Seq.min
+    let maxX = sensorAreas |> Seq.map (fun s -> (s.sensor |> fst) + s.range) |> Seq.max
+    let pointsCovered = 
+        [minX..maxX] 
+        |> Seq.choose (fun lineX ->
+            let lineP = (lineX, lineY)
+            sensorAreas |> Seq.tryFind (fun s -> isInsideSensorRange lineP s.sensor s.range))
+        |> Seq.length
+    // Count the sensors on this line, since obviously there IS a sensor at these coords
+    let beaconsOnLine =
+        sensorAreas 
+        |> Seq.map (fun s -> s.beacon)
+        |> Seq.distinct
+        |> Dump
+        |> Seq.filter (fun (x,y) -> y = lineY) // Filter out beacons ON the line
+        |> Seq.length
+
+    printfn "%i points covered, %i beacons on this line" pointsCovered beaconsOnLine
+    pointsCovered - beaconsOnLine
+
+
+getInput "Day15_sample1.txt" 
+|> solve1 10
--- Tools.fsx
+++ Tools.fsx
@@ -27,7 +27,8 @@ module TupleTools =
 
 [<AutoOpen>]
 module Distance =
-    let manhattanDistance (x1:int) (y1:int) (x2:int) (y2:int) = Math.Abs(x1 - x2) + Math.Abs(y1 - y2)
+    let inline manhattanDistance (x1:int) (y1:int) (x2:int) (y2:int) = Math.Abs(x1 - x2) + Math.Abs(y1 - y2)
+    let inline manhattanDistPoints (x1,y1) (x2,y2) = manhattanDistance x1 y1 x2 y2
 
 [<AutoOpen>]
 module RegexTools =
