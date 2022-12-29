d94978e235e49f2faacf3ce2054a9a4c1568361e
Julien Adam <julien.adam@gmail.com>
2022-12-29 23:14:02 +0100

    Day18 part 2 done
    Nothing to it, just started at 0,0,0 and explored every accessible cell
    in a slightly larger volume than the cube of side length 20 that contained the input, counting faces along the way
    
    Recursion, maintaining an open and closed list

--- Puzzles/2022/Day18.fsx
+++ Puzzles/2022/Day18.fsx
@@ -13,8 +13,6 @@ let getInput p =
     |> Set.ofSeq
     |> Dump
 
-getInput "Day18.txt"
-
 let enumAdjacentCoords3D (x,y,z) = seq {
     yield x+1,y,z
     yield x-1,y,z
@@ -28,3 +26,51 @@ let countAdjacentNeighbours3D grid coords =
     enumAdjacentCoords3D coords
     |> Seq.filter (fun c -> grid |> Set.contains c)
     |> Seq.length
+
+let solve1 grid =
+    grid |> Seq.map (fun c -> 6 - (countAdjacentNeighbours3D grid c)) |> Seq.sum
+
+    
+type Result =
+    | Face
+    | Explorable of (int*int*int)
+
+let solve2 grid = 
+    let outOfBounds i = i < -1 || i > 20
+
+    let rec countFaces toVisit visited grid facesFound =
+        if toVisit |> Set.isEmpty then
+            facesFound
+        else
+            let pos = toVisit |> Seq.head
+
+            let explorationResults = 
+                enumAdjacentCoords3D pos
+                |> Seq.map (fun (x,y,z) ->
+                    if outOfBounds x || outOfBounds y || outOfBounds z then
+                        None
+                    else if grid |> Set.contains (x,y,z) then
+                        Some Face
+                    else if visited |> Set.contains (x,y,z) then
+                        None
+                    else
+                        Some (Explorable (x,y,z))
+                )
+                |> Seq.toList
+
+            let newFacesFound = explorationResults |> Seq.map(fun r -> match r with | Some Face -> 1 | _ -> 0) |> Seq.sum
+            let nextToVisit = 
+                explorationResults
+                |> List.choose (fun r -> match r with | Some (Explorable p) -> Some p | _ -> None)
+                |> Set.ofList
+                |> Set.union toVisit
+                |> Set.remove pos
+
+            countFaces nextToVisit (visited |> Set.add pos) grid (newFacesFound + facesFound)
+    countFaces ([(0,0,0)] |> Set.ofList) Set.empty grid 0
+
+getInput "Day18.txt"
+|> Sol
+
+
+
