e0e4624cd5e72de91068a7b6cdba3adf47263de3
Julien Adam <julien.adam@gmail.com>
2022-12-31 13:49:08 +0100

    Day 21 parsing done

---
+++ Puzzles/2022/Day21.fsx
@@ -0,0 +1,50 @@
+﻿#time
+#load "../../Tools.fsx"
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+open Checked
+open Tools
+
+let regex = new Regex (@"(?<monkey>\w+)\: (((?<m1>\w+) (?<op>[\+\-\*\/]{1}) (?<m2>\w+))|(?<num>\d+))")
+type Op = int64 -> int64 -> int64
+
+type MonkeyYell =
+    | Number of int64
+    | MathOp of string * Op * string
+
+type Monkey = {
+    name: string
+    yell: MonkeyYell
+}
+
+let parseOp o =
+    match o with 
+    | "+" -> (+)
+    | "-" -> (-)
+    | "*" -> (*)
+    | "/" -> (/)
+    | _ -> failwithf "Invalid operator %s" o
+
+let parseMonkey line =
+    let m = regex.Match line
+    if not m.Success then
+        failwithf "Could not parse line %s" line
+    let yell = 
+        if m.Groups.["m1"].Success = false then
+           m|> mInt64 "num" |> Number
+        else
+            (m |> mStr "m1", m|>mStr "op" |> Dump |> parseOp, m |> mStr "m2") |> MathOp
+    { 
+        name = m |> mStr "monkey"
+        yell = yell
+    }
+
+let getInput p =
+    File.ReadAllLines(getInputPath2022 p)
+    |> Seq.map parseMonkey
+    |> Seq.toArray
+
+getInput "Day21.txt"
+//|> solve1
--- Tools.fsx
+++ Tools.fsx
@@ -52,6 +52,7 @@ module Distance =
 [<AutoOpen>]
 module RegexTools =
     let inline mInt (groupName:string) (m:Match) = m.Groups.[groupName].Value |> int
+    let inline mInt64 (groupName:string) (m:Match) = m.Groups.[groupName].Value |> int64
     let inline mStr (groupName:string) (m:Match) = m.Groups.[groupName].Value
 
 module AStar =
