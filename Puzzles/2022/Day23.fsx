cdeea1555354e632e20e605be03eb6bfb8297b92
Julien Adam <julien.adam@gmail.com>
2023-01-03 01:57:08 +0100

    Day 23 Done

--- Puzzles/2022/Day23.fsx
+++ Puzzles/2022/Day23.fsx
@@ -32,7 +32,7 @@ let round (directions:Direction list) (map:IDictionary<(int*int), bool>) =
         |> Seq.filter kvpValue 
         |> Seq.map kvpKey
         |> Seq.filter (fun pos -> 
-            // Filter elves that have al least one neighbour
+            // Filter elves that have at least one neighbour
             getAdjacentWithDiagonals pos map |> Seq.exists (fun (_,_,v) -> v.IsSome && v.Value)
         )
 
@@ -56,34 +56,50 @@ let round (directions:Direction list) (map:IDictionary<(int*int), bool>) =
                 proposed |> Option.bind (fun p -> Some ((r,c), p))
             )
         )
-    let validMoves = proposedMoves |> Seq.groupBy snd |> Seq.choose (fun (k, values) -> values |> Seq.tryExactlyOne) 
-    validMoves |> Seq.iter (fun ((elfPos), targetPos) -> 
-        map.[elfPos] <- false
+    let validMoves = 
+        proposedMoves 
+        |> Seq.groupBy snd 
+        |> Seq.choose (fun (_, values) -> values |> Seq.tryExactlyOne)
+        |> Seq.toList
+    validMoves 
+    |> Seq.iter (fun ((elfPos), targetPos) -> 
+        map.Remove(elfPos) |> ignore
         map.[targetPos] <- true
     )
     
-    map, List.append directions.Tail [directions.Head]
+    map, List.append directions.Tail [directions.Head], validMoves.IsEmpty
 
 let printer v = match v with | Some v when v -> '#' | _ -> '.'
 
 let solve1 rounds map =
-    printGrid printer map |> ignore
+    //printGrid printer map |> ignore
     let finalMap, _ =
         [1 .. rounds] |> Seq.fold (fun (map, directions) i ->
-            printfn "Round #%i, directions are %A" i directions
-            let map2, dir = round directions map
-            printGrid printer map2 |> ignore
+            //printfn "Round #%i, directions are %A" i directions
+            let map2, dir, _ = round directions map
+            //printGrid printer map2 |> ignore
             map2, dir
         ) (map, [North; South; West; East])
 
     Seq.allPairs [finalMap |> minR .. finalMap |> maxR] [finalMap |> minC .. finalMap |> maxC]
-    |> Seq.filter (fun pos -> match finalMap.TryGetValue(pos) with | true, v -> false | _ -> true)
+    |> Seq.filter (fun pos -> match finalMap.TryGetValue(pos) with | true, true -> false | _ -> true)
     |> Seq.length
-
-getInput "Day23_sample2.txt"
-|> solve1 4
-
 
 getInput "Day23_sample1.txt"
 |> solve1 10
 
+getInput "Day23.txt"
+|> solve1 10
+
+let solve2 map =
+    let rec solveRec roundNumber directions =
+        let _, dir, noMoves = round directions map
+        if noMoves then
+            roundNumber
+        else
+            solveRec (roundNumber + 1) dir
+
+    solveRec 1 [North; South; West; East]
+
+getInput "Day23.txt"
+|> solve2
