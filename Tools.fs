6487d2dce4218e98d35e624289c547853b57a943
Julien Adam <julien.adam@gmail.com>
2023-12-01 11:37:35 +0100

    Some tools for loading files

--- Tools.fs
+++ Tools.fs
@@ -11,7 +11,11 @@ module Tools =
     let getInputPath file = Path.Combine(__SOURCE_DIRECTORY__, "Input", "2021", file)
     let getInputPath2022 file = Path.Combine(__SOURCE_DIRECTORY__, "Input", "2022", file)
     let getInputPath2023 file = Path.Combine(__SOURCE_DIRECTORY__, "Input", "2023", file)
-    
+    let getInputPathFsx (sourceDir:string) (sourceFile:string) qualifier =
+        let year = DirectoryInfo(sourceDir).Name
+        let fileName = sprintf "%s%s.txt" (Path.GetFileNameWithoutExtension(sourceFile)) qualifier
+        Path.Combine(__SOURCE_DIRECTORY__, "Input", year, fileName)
+
     let Dump obj =
         printfn "%A" obj
         obj
