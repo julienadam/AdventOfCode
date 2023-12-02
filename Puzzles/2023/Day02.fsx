a1af0e9a665342001dd2cd90889733fee641ffbd
Julien Adam <julien.adam@gmail.com>
2023-12-02 10:32:49 +0100

    Day02 part 1 done

--- Puzzles/2023/Day02.fsx
+++ Puzzles/2023/Day02.fsx
@@ -1,4 +1,3 @@
-
 #time
 #load "../../Tools.fs"
 
@@ -6,24 +5,41 @@ open System
 open System.IO
 open AdventOfCode
 
-type Color = | Red | Green | Blue
-
-let getInput p = File.ReadAllLines(getInputPath2023 p)
+type Show = {
+    Red : int
+    Green : int
+    Blue : int
+}
 
 let parse line =
-    let inline parseColor color = match color with | "red" -> Color.Red | "green" -> Color.Green | "blue" -> Color.Blue | _ -> failwithf "invalid color %s" color
     let _, contents = line |> ssplit2 ": "
     contents 
     |> ssplit "; " 
     |> Array.map (fun show -> 
         show 
         |> ssplit ", "
-        |> Array.map (split2 ' ')
-        |> Array.map (fun (qty, color) ->
-            qty |> int, parseColor color
-        )
+        |> Array.fold 
+            (fun show str ->
+                let qty, color = str |> split2 ' '
+                match color with 
+                | "red" -> { show with Red = qty |> int }
+                | "green" -> { show with Green = qty |> int }
+                | "blue" -> { show with Blue = qty |> int }
+                | _ -> failwithf "invalid color %s" color
+            )
+            { Red = 0; Green = 0; Blue = 0 }
     )
 
-getInput "Day02.txt"
-|> Array.map parse
-|> Dump
+let getInput p = 
+    File.ReadAllLines(getInputPath2023 p) 
+    |> Array.mapi (fun i l -> i + 1, parse l)
+
+
+let solve1 input =
+    getInput input
+    |> Seq.filter(fun (_, shows) ->
+        shows |> Seq.forall (fun show -> show.Red <= 12 && show.Green <= 13 && show.Blue <= 14)
+    )
+    |> Seq.sumBy fst
+
+solve1 "Day02.txt"
