0deb6d53312facdd2c8073d41ab92c15435615fa
Julien Adam <julien.adam@gmail.com>
2023-12-04 12:26:05 +0100

    Day 04 done

--- Puzzles/2023/Day04.fsx
+++ Puzzles/2023/Day04.fsx
@@ -16,11 +16,13 @@ let getInput p =
     File.ReadAllLines(getInputPath2023 p)
     |> Array.mapi (fun i l -> i + 1, parseLine l)
 
+let countWins (winningNumbers: Set<int>) (myNumbers: int list) =
+    myNumbers 
+    |> Seq.filter (fun n -> winningNumbers |> Set.contains n) 
+    |> Seq.length
+
 let cardScore (winningNumbers: Set<int>) (myNumbers: int list) =
-    let wins = 
-        myNumbers 
-        |> Seq.filter (fun n -> winningNumbers |> Set.contains n) 
-        |> Seq.length
+    let wins = countWins winningNumbers myNumbers
     if wins > 0 then pow2 (wins - 1) else 0
 
 let solve1 input = 
@@ -31,4 +33,57 @@ let solve1 input =
     |> Seq.toList
     |> Seq.sum
 
+let solve2 input =
+    
+    let countWinsForEachCard input =
+        getInput input
+        |> Seq.map (fun (cardNo, (winners, numbers)) -> cardNo, countWins winners numbers)
+        |> Seq.toList
+    
+    let cardWins = countWinsForEachCard input
+
+    let getCardsWon cardNo numWins =
+        if cardNo + numWins <= cardWins.Length then
+            // Enough cards left
+            [(cardNo + 1)..(cardNo + numWins)]
+        else if cardNo < input.Length then
+            // Some remaining, but not all
+            [(cardNo + 1)..(min cardWins.Length (cardNo + numWins))]
+        else
+            // No cards left
+            []
+
+    // Card ids and the list of card ids it will win
+    let winsByCard = 
+        cardWins 
+        |> Seq.map (fun (cardNo, wins) -> cardNo, getCardsWon cardNo wins) 
+
+    // Initial state, one copy of each
+    let state = 
+        cardWins
+        |> Seq.map (fun (cardNo, _) -> cardNo, 1)
+        |> Map.ofSeq
+
+    // For each card id that is won, add count number of copies to the state
+    let applyWins (count:int) (wins:int list) (cardCounts:Map<int, int>) =
+        wins 
+        |> Seq.fold (fun (acc:Map<int,int>) winningCardId ->
+            let currentCount = acc[winningCardId]
+            acc |> Map.add winningCardId (currentCount + count)) cardCounts
+
+    // Scan all card ids and apply the winnings
+    let finalCounts = 
+        winsByCard 
+        |> Seq.fold (fun (acc:Map<int, int>) (id, wins) -> 
+            acc 
+            |> applyWins acc[id] wins
+        ) state
+
+    finalCounts 
+    |> Map.toSeq
+    |> Seq.map snd
+    |> Seq.sum
+
 solve1 "Day04.txt"
+solve2 "Day04.txt"
+
