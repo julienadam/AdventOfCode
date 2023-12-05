4b235e6d00f02b05001e5d0c07104a5a9fca6c86
Julien Adam <julien.adam@gmail.com>
2023-12-05 13:55:28 +0100

    Day 05 Finished

--- Puzzles/2023/Day05.fsx
+++ Puzzles/2023/Day05.fsx
@@ -1,26 +1,73 @@
 
 #time
 #load "../../Tools.fs"
+#r "nuget: Faqt"
+
 
 open System
 open System.IO
 open AdventOfCode
 open Ranges
 
+type ConversionResult = 
+    | Unchanged of Range64
+    | Converted of Range64
+
 type ConversionRange = {
-    SourceRange : Range64
-    DestStart : int64
+    Source : Range64
+    Dest : Range64
 }
 with 
     member this.Convert(x: int64) = 
-        match this.SourceRange.PositionInRange x with
-        | Some pos -> this.DestStart + pos
+        match this.Source.PositionInRange x with
+        | Some pos -> this.Dest.Start + pos
         | _ -> x
+    member this.Convert(input: Range64) : ConversionResult list=
+        if input.Start > this.Source.End then
+            // |SS---SE|---|IS---IE|
+            [input |> Unchanged]
+        else if input.End < this.Source.Start then
+            // |IS---IE|---|SS---SE|
+            [input |> Unchanged]
+        else
+            if input.IsInRange(this.Source.Start) then
+                if input.IsInRange(this.Source.End) then
+                    // |IS---|SS---SE|---IE|
+                    [
+                        if this.Source.Start > input.Start then
+                            yield { Start = input.Start; End = this.Source.Start - 1L} |> Unchanged
+                        yield this.Dest |> Converted// Converted range
+                        if input.End > this.Source.End then
+                            yield { Start = this.Source.End + 1L; End = input.End} |> Unchanged
+                    ]
+                else
+                    // |IS---|SS---IE|---SE|
+                    [
+                        if input.Start < this.Source.Start then
+                            yield { Start = input.Start; End = this.Source.Start - 1L} |> Unchanged
+                        yield { Start = this.Dest.Start; End = this.Convert input.End} |> Converted
+                    ]
+            else if this.Source.IsInRange(input.Start) then
+                if this.Source.IsInRange(input.End) then
+                    // |SS---|IS---IE|---SE|
+                    [
+                        { Start = this.Convert(input.Start); End = this.Convert(input.End)} |> Converted
+                    ]
+                else
+                    // |SS---|IS---SE|---IE|
+                    [
+                        yield { Start = this.Convert(input.Start); End = this.Dest.End} |> Converted
+                        if input.End > this.Source.Start then
+                            yield { Start = this.Source.End + 1L; End = input.End} |> Unchanged
+                    ]
+            else
+                failwithf "Invalid range intersection"
+
 
 let intsToConversion (values:int64 array) = 
     { 
-        DestStart = values.[0]
-        SourceRange = { Start = values.[1]; End = values.[1] + values.[2] - 1L }
+        Dest = { Start = values.[0]; End = values.[0]+ values.[2] - 1L }
+        Source = { Start = values.[1]; End = values.[1] + values.[2] - 1L }
     }
 
 let readMapBlock block =
@@ -48,7 +95,7 @@ let getInput name =
     seeds, maps
 
 let inline applyRangeConversions value (ranges: ConversionRange list) =
-    let conv = ranges |> List.tryFind (fun r -> r.SourceRange.IsInRange value)
+    let conv = ranges |> List.tryFind (fun r -> r.Source.IsInRange value)
     match conv with
     | Some c -> c.Convert value
     | _ -> value    //|> Seq.fold (fun v range -> range.Convert v) value
@@ -67,35 +114,76 @@ let solve1 input =
 
     applyStages "seed" seeds |> Dump |> Seq.min
 
-solve1 "Day05_sample.txt"
-solve1 "Day05.txt"
 
+type State = {
+    Todo: Range64 list
+    Done: Range64 list
+}
 
-// Tests
-// let conv = intsToConversion [| 50;98;2 |] |> Dump
-// conv.Convert 97
-// conv.Convert 98
-// conv.Convert 99
-// conv.Convert 100
+let applyRangeConversionsToRange (value:Range64) (ranges: ConversionRange list) =
+    let allConverted = 
+        ranges |> List.fold (fun state range ->
+            // printfn "State %A. Range %A" state range
+            if state.Todo.IsEmpty then 
+                state
+            else 
+                let result = state.Todo |> List.collect range.Convert
+                let converted, todo = result |> List.partition (fun r -> match r with | Converted _ -> true | _ -> false)
+                // printfn "*** result"
+                // converted |> Dump|> ignore
+                todo |> ignore 
+                {
+                    Todo = todo |> List.map (fun r -> match r with | Unchanged u -> u | _ -> failwithf "Not an unchanged")
+                    Done = 
+                        List.concat [
+                            state.Done 
+                            converted |> List.map (fun r -> match r with | Converted c -> c | _ -> failwithf "Not a converted") 
+                        ] 
+                }
+        ) { Todo = [value]; Done = []}
+    // allConverted |> Dump |> ignore
+    List.concat [allConverted.Todo; allConverted.Done]
 
-// let ranges = [ 
-//     intsToConversion [| 50;98;2 |]
-//     intsToConversion [| 52;50;48 |]
-// ]
+let solve2 input =
+    let seeds, maps = getInput input
+    let seedRanges = 
+        seeds 
+        |> Seq.chunkBySize 2
+        |> Seq.map (fun r -> {Start = r[0]; End = r[0] + r[1] - 1L })
+        |> List.ofSeq
+    
+    let rec applyStages stage ranges =
+        stage |> Dump |> ignore
+        ranges |> Dump |> ignore
+        match stage with
+        | "location" -> ranges
+        | intermediate -> 
+            let dest, conversionRanges = maps[intermediate]
+            let converted = ranges |> List.collect (fun s -> applyRangeConversionsToRange s conversionRanges)
+            applyStages dest converted
 
-// applyRangeConversions 79 ranges
-// applyRangeConversions 14 ranges
-// applyRangeConversions 55 ranges
-// applyRangeConversions 13 ranges
+    applyStages "seed" seedRanges
+    |> Seq.map (fun r -> r.Start)
+    |> Seq.min
 
-// let ranges = [ 
-//     intsToConversion [|49;53;8|]
-//     intsToConversion [|0;11;42|]
-//     intsToConversion [|42;0;7|]
-//     intsToConversion [|57;7;4|]
-// ]
+solve2 "Day05_sample.txt"
+solve2 "Day05.txt"
 
-// ranges |> List.map (fun conv -> conv.SourceRange.IsInRange 53)
+open Faqt
 
-
-// applyRangeConversions 53 ranges
+let conv = intsToConversion [|50;100;4|]
+(conv.Convert {Start = 0; End = 99}).Should().SequenceEqual([{Start = 0; End = 99} |> Unchanged])
+(conv.Convert {Start = 0; End = 100}).Should().SequenceEqual([{Start = 0; End = 99} |> Unchanged; {Start = 50; End = 50} |> Converted])
+(conv.Convert {Start = 0; End = 101}).Should().SequenceEqual([{Start = 0; End = 99} |> Unchanged; {Start = 50; End = 51} |> Converted])
+(conv.Convert {Start = 0; End = 103}).Should().SequenceEqual([{Start = 0; End = 99} |> Unchanged; {Start = 50; End = 53} |> Converted])
+(conv.Convert {Start = 0; End = 104}).Should().SequenceEqual([{Start = 0; End = 99} |> Unchanged; {Start = 50; End = 53} |> Converted; {Start = 104; End = 104} |> Unchanged])
+(conv.Convert {Start = 0; End = 200}).Should().SequenceEqual([{Start = 0; End = 99} |> Unchanged; {Start = 50; End = 53} |> Converted; {Start = 104; End = 200} |> Unchanged])
+(conv.Convert {Start = 100; End = 101}).Should().SequenceEqual([{Start = 50; End = 51} |> Converted])
+(conv.Convert {Start = 101; End = 102}).Should().SequenceEqual([{Start = 51; End = 52} |> Converted])
+(conv.Convert {Start = 100; End = 103}).Should().SequenceEqual([{Start = 50; End = 53} |> Converted])
+(conv.Convert {Start = 100; End = 104}).Should().SequenceEqual([{Start = 50; End = 53} |> Converted; {Start = 104; End = 104} |> Unchanged])
+(conv.Convert {Start = 100; End = 200}).Should().SequenceEqual([{Start = 50; End = 53} |> Converted; {Start = 104; End = 200} |> Unchanged])
+(conv.Convert {Start = 101; End = 200}).Should().SequenceEqual([{Start = 51; End = 53} |> Converted; {Start = 104; End = 200} |> Unchanged])
+(conv.Convert {Start = 103; End = 200}).Should().SequenceEqual([{Start = 53; End = 53} |> Converted; {Start = 104; End = 200} |> Unchanged])
+(conv.Convert {Start = 104; End = 200}).Should().SequenceEqual([{Start = 104; End = 200} |> Unchanged])
+(conv.Convert {Start = 200; End = 300}).Should().SequenceEqual([{Start = 200; End = 300} |> Unchanged])
--- Tools.fs
+++ Tools.fs
@@ -51,16 +51,16 @@ module Tools =
     let inline pow2 power = 1 <<< power
         
 module Ranges =
-    type Range = {
-        Start: int
-        End: int
-    }
-    with
-        member this.IsInRange x = x >= this.Start && x <= this.End
-        member this.PositionInRange x = 
-            match this.IsInRange x with
-            | true -> Some (x - this.Start)
-            | false -> None
+    // type Range<> = {
+    //     Start: int
+    //     End: int
+    // }
+    // with
+    //     member this.IsInRange x = x >= this.Start && x <= this.End
+    //     member this.PositionInRange x = 
+    //         match this.IsInRange x with
+    //         | true -> Some (x - this.Start)
+    //         | false -> None
 
     type Range64 = {
         Start: int64
@@ -72,7 +72,24 @@ module Ranges =
             match this.IsInRange x with
             | true -> Some (x - this.Start)
             | false -> None
-
+        member this.Intersection other =
+            if other.Start > this.End then
+                None
+            else if other.End < this.Start then
+                None
+            else
+                if other.IsInRange(this.Start) then
+                    if other.IsInRange(this.End) then
+                        Some this
+                    else
+                        Some { Start = this.Start; End = other.End }
+                else if this.IsInRange(other.Start) then
+                    if this.IsInRange(other.End) then
+                        Some other
+                    else
+                        Some { Start = other.Start; End = this.End }
+                else
+                    failwithf "Invalid range intersection"
 
 module SeqEx =
     let read n s =
