95d722afd23d3b59892de55dce2cf25944e11fbd
Julien Adam <julien.adam@gmail.com>
2023-12-09 10:50:20 +0100

    Day09 part 2 done

--- Puzzles/2023/Day09.fsx
+++ Puzzles/2023/Day09.fsx
@@ -1,10 +1,7 @@
-
 #time
 #load "../../Tools.fs"
 #r "nuget: faqt"
 
-
-open System
 open System.IO
 open AdventOfCode
 
@@ -34,8 +31,24 @@ let solve1 input =
     |> Seq.map computeNext
     |> Seq.sum
 
-open Faqt
+solve1 "Day09_sample.txt"
+solve1 "Day09.txt"
 
+let inline computeNext2 (steps: int list list) =
+    steps 
+    |> Seq.map (fun s -> s |> List.head)
+    |> Seq.fold (fun curr firstOfLine -> firstOfLine - curr) 0
+
+let solve2 input =
+    getInput input 
+    |> Seq.map stepsTo0
+    |> Seq.map computeNext2
+    |> Seq.sum
+
+solve2 "Day09_sample.txt"
+solve2 "Day09.txt"
+
+open Faqt
 
 (stepsTo0 [0;3;6;9;12;15]).Should().SequenceEqual([[3;3;3;3;3]; [0;3;6;9;12;15]])
 (stepsTo0 [1;3;6;10;15;21]).Should().SequenceEqual([[1;1;1;1]; [2;3;4;5;6]; [1;3;6;10;15;21]])
@@ -43,4 +56,6 @@ open Faqt
 ([1;3;6;10;15;21] |> stepsTo0 |> computeNext).Should().Be(28)
 ([10;13;16;21;30;45] |> stepsTo0 |> computeNext).Should().Be(68)
 
-solve1 "Day09.txt"
+([0;3;6;9;12;15] |> stepsTo0 |> computeNext2).Should().Be(-3)
+([1;3;6;10;15;21] |> stepsTo0 |> computeNext2).Should().Be(0)
+([10;13;16;21;30;45] |> stepsTo0 |> computeNext2).Should().Be(5)
