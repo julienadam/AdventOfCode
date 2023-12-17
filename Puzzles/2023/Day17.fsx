a9ccb8377a533cac2143f0f20a5a6c9a45457bb0
Julien Adam <julien.adam@gmail.com>
2023-12-17 12:45:54 +0100

    Day 17 part 1 done
    Ended up doing a BFS with priority queue

--- Puzzles/2023/Day17.fsx
+++ Puzzles/2023/Day17.fsx
@@ -1,7 +1,6 @@
 #time "on"
 #load "../../Tools.fs"
 #load "../../Tools/Array2dTools.fs"
-#load "../../Tools/AStar.fs"
 #load "../../Tools/Distance.fs"
 #load "../../Tools/Directions.fs"
 
@@ -9,15 +8,13 @@ open System
 open System.IO
 open AdventOfCode
 open Array2DTools
-open FullAStar
+open System.Collections.Generic
 
 let getInput name = 
     File.ReadAllLines(getInputPath2023 name)
     |> Array.map (fun line -> line |> Seq.map (fun c -> (c - '0') |> int) |> Seq.toArray)
     |> array2D
 
-[<CustomComparison>]
-[<CustomEquality>]
 type State = 
     {
         row: int
@@ -25,18 +22,6 @@ type State =
         dir: Direction option
         straight: int
     }
-    override this.Equals other =
-        match other with
-        | :? State as s -> this.row = s.row && this.col = s.col
-        | _ -> false
-    override this.GetHashCode() = (this.row, this.col).GetHashCode()
-    interface IComparable with
-        member this.CompareTo(other:obj) =
-            match other with
-            | :? State as s -> 
-                ValueTuple<int,int>(this.row, this.col).CompareTo(s.row, s.col)
-            | _ -> 0
-
 
 let solve1 input =
     let grid = getInput input
@@ -45,25 +30,29 @@ let solve1 input =
         match s.dir with
         | Some North -> 
             [
-                if s.straight < 3 then yield { row = s.row-1; col =s.col; dir =Some North; straight = s.straight+1 }
+                if s.straight < 3 then 
+                    yield { row = s.row-1; col =s.col; dir =Some North; straight = s.straight+1 }
                 yield { row = s.row; col = s.col-1; dir = Some West; straight = 1 }
                 yield { row = s.row; col = s.col+1; dir = Some East; straight = 1 }
             ]
         | Some South -> 
             [
-                if s.straight < 3 then yield { row = s.row+1; col = s.col; dir = Some South; straight =s.straight+1 }
+                if s.straight < 3 then 
+                    yield { row = s.row+1; col = s.col; dir = Some South; straight =s.straight+1 }
                 yield { row = s.row; col = s.col-1; dir = Some West; straight = 1 }
                 yield { row = s.row; col = s.col+1; dir = Some East; straight = 1 }
             ]
         | Some East ->
             [
-                if s.straight < 3 then yield { row = s.row; col = s.col+1; dir = Some East; straight = s.straight+1 }
+                if s.straight < 3 then 
+                    yield { row = s.row; col = s.col+1; dir = Some East; straight = s.straight+1 }
                 yield { row = s.row-1; col = s.col; dir = Some North; straight =1 }
                 yield { row = s.row+1; col = s.col; dir = Some South; straight =1 }
             ]
         | Some West ->  
             [
-                if s.straight < 3 then yield { row = s.row; col = s.col-1; dir = Some West; straight =s.straight+1 }
+                if s.straight < 3 then 
+                    yield { row = s.row; col = s.col-1; dir = Some West; straight =s.straight+1 }
                 yield { row = s.row-1; col = s.col; dir = Some North; straight =1 }
                 yield { row = s.row+1; col = s.col; dir = Some South; straight =1 }
             ]
@@ -81,27 +70,26 @@ let solve1 input =
             getPossibleNextCells s 
             |> Seq.filter (fun s -> filterInvalidCoords s.row s.col)
 
-    let config: Config<State> = {
-        fCost = (fun s1 s2 -> manhattanDistance s1.row s1.col s2.row s2.col)
-        gCost = (fun s1 s2 -> grid[s2.row, s2.col])
-        neighbours = getNeighbours
-        maxIterations = None
-    }
+    let seen = new HashSet<State>()
+    let rec heatlossRec (q:PriorityQueue<State,int>)  =
+        match q.TryDequeue() with
+        | true, s, hl -> 
+            if s.row = (grid |> maxR) && s.col = (grid |> maxC) then
+                Some hl
+            else
+                getNeighbours(s)
+                |> Seq.iter(fun n ->
+                    if seen.Contains(n) = false then
+                        seen.Add(n) |> ignore
+                        q.Enqueue(n, hl + grid[n.row, n.col])
+                )
+                heatlossRec q
+        | _ -> 
+            None
 
-    let start = { row = 0; col = 0; dir = None; straight = 0 }
-    let target = { row = grid |> maxR; col = grid |> maxC; dir = None; straight = 0 }
-    match search start target config with
-    | Some cells -> 
-        let totalCost = 
-            cells 
-            |> Seq.filter (fun s -> s.row <> 0 || s.col <> 0)
-            |> Seq.map (fun s -> grid[s.row, s.col]) |> Seq.sum
-        totalCost
-    | None -> 
-        failwithf "No solution found"
+    let q = new PriorityQueue<State, int>()
+    q.Enqueue({ row = 0; col = 0; dir = None; straight = 0 }, 0);
+    heatlossRec q
 
-// 1095 too high
-// 838 too high
-// 803 too high
 solve1 "Day17_sample1.txt"
 solve1 "Day17.txt"
