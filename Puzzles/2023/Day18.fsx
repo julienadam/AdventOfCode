96fcc6150d25f169fe2315e8b50d980523253060
Julien Adam <julien.adam@gmail.com>
2023-12-18 17:40:25 +0100

    Day 18 done

--- Puzzles/2023/Day18.fsx
+++ Puzzles/2023/Day18.fsx
@@ -1,87 +1,89 @@
 #time "on"
 #load "../../Tools.fs"
 #load "../../Tools/Directions.fs"
-#load "../../Tools/SparseGrid.fs"
+#load "../../Tools/Distance.fs"
 #r "nuget:NFluent"
 
+open System
 open System.IO
-open System.Drawing
 open AdventOfCode
-open System.Collections.Generic
-open SparseGrid
 open NFluent
 
-let parseLine line =
-    let split = line |> ssplit " "
-    let dir = 
-        match split[0] with 
-        | "U" -> North
-        | "D" -> South
-        | "L" -> West
-        | "R" -> East
-        | _ -> failwithf "invalid dir"
-    let length = split[1] |> int
-    dir, length
+// Math stuff
+// Shoelace formula : https://en.wikipedia.org/wiki/Shoelace_formula
+//  Gives the interior area
+// Pick's theorem   : https://en.wikipedia.org/wiki/Pick%27s_theorem
+//  Compute the area given the interior area and the number of points on the edges
 
 let getInput name =
+    let parseLine line =
+        let split = line |> ssplit " "
+        let dir = 
+            match split[0] with 
+            | "U" -> North
+            | "D" -> South
+            | "L" -> West
+            | "R" -> East
+            | _ -> failwithf "invalid dir"
+        let length = split[1] |> int
+        dir, length
+
     File.ReadAllLines(getInputPath2023 name)
     |> Array.map parseLine
 
-let digTrench (instructions:(Direction * int) seq) =
-    let grid = new Dictionary<(int*int), unit>()
-    instructions 
-    |> Seq.fold(fun (row, col) (dir, len) ->
-        [1..len] |> Seq.fold(fun (r,c) _ -> 
-            let newPos = dir.Move (r, c)
-            grid.Add(newPos, ()) |> ignore
-            newPos
-        ) (row, col)
-    ) (0,0)
-    |> ignore
+let vertices instructions =
+    instructions
+    |> Seq.fold (fun acc (dir:Direction, len) ->
+        let (dr, dc) = dir.MoveBy(acc |> List.head, len)
+        (dr, dc) :: acc
+    ) [(0,0)]
 
-    grid
+let shoelace (points: (int*int) list) =
+    // Put the head at the back, this will give us a list of vertices 
+    // offset by one
+    let next = List.append (points |> List.tail) [points |> List.head]
 
-let digInterior (grid:IDictionary<(int*int), unit>) =
-    let minR = grid |> minR
-    let maxR = grid |> maxR
-    let minC = grid |> minC
-    let maxC = grid |> maxC
-    for r = minR to maxR do
-        let mutable enteredDir = None
-        let mutable walls = 0
-        for c = minC to maxC do
-            let isEdge = grid.ContainsKey(r,c)
-            match isEdge, enteredDir with
-            | false, _ -> 
-                enteredDir <- None
-                if walls % 2 = 1 then
-                    grid.Add((r,c), ()) |> ignore
-            | true, None ->
-                match grid.ContainsKey(r-1,c), grid.ContainsKey(r+1,c) with
-                | true, true -> walls <- walls + 1
-                | false, false -> failwithf "Single cell edge not supported at %i,%i" r c
-                | true, false -> enteredDir <- Some North
-                | false, true -> enteredDir <- Some South
-            | true, Some dir  ->
-                match dir, grid.ContainsKey(r-1,c), grid.ContainsKey(r+1,c) with
-                | _, true, true
-                | North, false, true
-                | South, true, false -> walls <- walls + 1
-                | _, false, false
-                | North, true, false
-                | South, false, true -> ()
-                | x -> failwithf "Unexpected interior configuration %A at %i,%i" x r c
-    grid
+    // Apply the formula
+    let total = 
+        List.zip points next
+        |> List.sumBy(fun ((x1,y1), (x2,y2)) ->
+            (x1 |> int64)*(y2 |> int64) - (x2 |> int64)*(y1 |>int64)
+        )
 
-let solve1 input =
-    let digSite = 
-        getInput input 
-        |> digTrench
-        |> digInterior
+    abs(total / 2L)
 
-    digSite.Count
+/// External perimeter of point list
+let exterior (points: (int*int) list) =
+    points 
+    |> Seq.pairwise 
+    |> Seq.map (fun (p1,p2) -> (manhattanDistPoints p1 p2))
+    |> Seq.sumBy int64
+
+let digSize vertices = (vertices |> shoelace) + ((vertices |> exterior) / 2L + 1L)
+
+let solve1 fileName = getInput fileName |> vertices |> digSize
 
 Check.That(solve1 "Day18_sample1.txt").IsEqualTo(62)
-
 solve1 "Day18.txt"
 
+let getInput2 name =
+    let parseLine (line:string) =
+        let hexColor = line.Substring(line.IndexOf('#') + 1, 6)
+        let dir = 
+            match hexColor[5] with
+            | '0' -> East
+            | '1' -> South
+            | '2' -> West
+            | '3' -> North
+            | x -> failwithf "Not a valid direction %A" x
+
+        dir, Int32.Parse(hexColor.Substring(0,5), Globalization.NumberStyles.HexNumber)
+
+    File.ReadAllLines(getInputPath2023 name)
+    |> Array.map parseLine
+
+
+let solve2 fileName = getInput2 fileName |> vertices |> digSize
+
+Check.That(solve2 "Day18_sample1.txt").IsEqualTo(952408144115L)
+solve2 "Day18.txt"
--- Tools/Directions.fs
+++ Tools/Directions.fs
@@ -4,6 +4,8 @@ namespace AdventOfCode
 module Directions =
 
     let inline (++) (a,b) (c,d) = (a+c, b+d)
+    let inline mul (a,b) mult = (a*mult, b*mult)
+
     let north = (-1,0)
     let south = (1,0)
     let east = (0,1)
@@ -18,13 +20,17 @@ module Directions =
         member this.TurnLeft () = match this with | North -> West | East -> North | South -> East | West -> South
         member this.TurnRight () = match this with | North -> East | East -> South | South ->  West | West ->  North
         member this.Move(p:int*int) =
-            
             match this with
             | North -> p ++ north
             | South -> p ++ south
             | East -> p ++ east
             | West -> p ++ west
-
+        member this.MoveBy(p:int*int, dist:int) =
+            match this with
+            | North -> p ++ (mul north dist)
+            | South -> p ++ (mul south dist)
+            | East -> p ++ (mul east dist)
+            | West -> p ++ (mul west dist)
     type Compass =
     | Up
     | Down
