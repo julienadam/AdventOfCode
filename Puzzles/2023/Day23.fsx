561024197fe0ce95f0a9dca4c4ff5cdd67c5259f
Julien Adam <julien.adam@gmail.com>
2023-12-25 17:48:49 +0100

    Day 23 finished

--- Puzzles/2023/Day23.fsx
+++ Puzzles/2023/Day23.fsx
@@ -1,19 +1,27 @@
 #time "on"
 #load "../../Tools.fs"
 #load "../../Tools/Array2DTools.fs"
+#r "nuget: FSharp.Collections.ParallelSeq"
 #r "nuget: NFluent"
 
 open System.IO
 open AdventOfCode
 open AdventOfCode.Array2DTools
+open FSharp.Collections.ParallelSeq
 open NFluent
+open System.Collections.Generic
 
 let getInput name = 
-    File.ReadAllLines(getInputPath2023 name)
-    |> Array.map (fun line -> line.ToCharArray())
-    |> array2D
+    let grid = 
+        File.ReadAllLines(getInputPath2023 name)
+        |> Array.map (fun line -> line.ToCharArray())
+        |> array2D
+    // Plug the entrance and exit to avoid bound checks
+    grid[0,1] <- '#'
+    grid[(grid |> maxR), ((grid |> maxC) - 1)] <- '#'
+    grid
 
-let getAdjacent (grid:char array2d) r c =
+let getAdjacentWithSlopes (grid:char array2d) r c =
     match grid[r,c] with
     | '>' -> [ r, c + 1 ]
     | '<' -> [ r, c - 1 ]
@@ -22,15 +30,13 @@ let getAdjacent (grid:char array2d) r c =
     | '.' -> [ r - 1, c; r + 1, c; r, c - 1; r, c + 1 ]
     | _ -> failwith "error"
     
-let solve1 input =
+let solve getAdjacent input =
     let grid = getInput input 
-    // Plug the entrance and exit to avoid bound checks
-    grid[0,1] <- '#'
-    grid[(grid |> maxR), ((grid |> maxC) - 1)] <- '#'
     let target = (grid |> maxR) - 1, (grid |> maxC) - 1
-    
+
     let rec walk r c path =
         let p = path |> Set.add (r,c)
+        // printfn "%i,%i" r c
         if (r,c) = target then
             p
         else
@@ -42,13 +48,97 @@ let solve1 input =
                     Set.empty
                 else
                     adjacent 
-                    |> Seq.map(fun (ar,ac) -> walk ar ac p)
-                    |> Seq.maxBy(fun p -> p.Count)
+                    |> List.map(fun (ar,ac) -> walk ar ac p)
+                    |> List.maxBy(fun p -> p.Count)
 
     let longestPath = walk 1 1 Set.empty
     longestPath.Count + 1
 
-Check.That(solve1 "Day23_sample1.txt").IsEqualTo(94)
+let solve1 = solve getAdjacentWithSlopes
 
-solve1 "Day23.txt"
+// Check.That(solve1 "Day23_sample1.txt").IsEqualTo(94)
+// solve1 "Day23.txt"
 
+let getAdjacentWithoutSlopes (grid:char array2d) r c =
+    match grid[r,c] with
+    | '>'
+    | '<'
+    | 'v'
+    | '^'
+    | '.' -> [ r - 1, c; r + 1, c; r, c - 1; r, c + 1 ]
+    | x -> failwithf "found %c at %i,%i" x r c
+
+#r "nuget: QuikGraph"
+#r "nuget: QuikGraph.GraphViz"
+
+open QuikGraph
+open QuikGraph.Algorithms
+
+let solve2 name = 
+
+    let grid = getInput name
+
+    let buildGraph grid getAdjacent = 
+        let graph = new AdjacencyGraph<(int*int), TaggedEdge<(int*int), int>>();
+        let visited = new HashSet<int*int>()
+        let toWalk = new Queue<(int*int)*(int*int)*int>()
+        let target = (grid |> maxR) - 1, (grid |> maxC) - 1
+
+        // let ensureEdge (r,c) =
+        //     if graph.ContainsVertex(r,c) = false then
+        //         graph.AddVertex(r,c) |> ignore
+
+        let rec walk () =
+            match toWalk.TryDequeue() with
+            | true, (n, s, l) when n = target-> 
+                printfn "Found exit"
+                graph.AddVertex(s) |> ignore
+                graph.AddVertex(n) |> ignore
+                graph.AddEdge(new TaggedEdge<(int*int), int>(s, n, l + 1)) |> ignore
+                ()
+            | true, ((r,c), (sr,sc), l) -> 
+                visited.Add(r,c) |> ignore
+                let adjacent = 
+                    getAdjacent grid r c
+                    |> List.filter (fun (ar, ac) -> grid[ar,ac] <> '#')
+                match adjacent with
+                | [a;b] when visited.Contains(a) ->
+                    toWalk.Enqueue(b, (sr,sc), l + 1) |> ignore
+                | [a;b] when visited.Contains(b) ->
+                    toWalk.Enqueue(a, (sr,sc), l + 1) |> ignore
+                | [] -> 
+                    ()
+                | next ->
+                    printfn "Adding edge from (%i,%i) to (%i,%i) with length %i" sr sc r c (l+1)
+                    graph.AddVertex((sr,sc)) |> ignore
+                    graph.AddVertex((r,c)) |> ignore
+                    graph.AddEdge(new TaggedEdge<(int*int), int>((sr,sc), (r,c), l + 1)) |> ignore
+                    next 
+                    |> List.filter (fun n -> visited.Contains(n) = false)
+                    |> List.iter (fun n -> toWalk.Enqueue(n, (r,c), 1))
+
+                walk()
+            | false, _ ->
+                printfn "List is empty"
+                ()
+
+        toWalk.Enqueue((1,1), (1,1), 0)
+        walk ()
+
+        graph
+
+
+    let graph = buildGraph grid getAdjacentWithoutSlopes
+    let graphViz = new Graphviz.GraphvizAlgorithm<(int*int), TaggedEdge<(int*int), int>>(graph)
+    let dotGraph = graphViz.Generate()
+    File.WriteAllText(@"f:\temp\23.dot", dotGraph)
+    graph
+
+solve2 "Day23_sample1.txt"
+
+    // let reduced  = graph.ComputeTransitiveClosure()
+    
+    
+    
+
+    
