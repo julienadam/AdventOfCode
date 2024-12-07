995536e9941f2829477b41b680fc8bfeae7d52bb
Julien Adam <julien.adam@arcodia.fr>
2024-12-07 17:20:17 +0100

    Finished Day 07

--- Puzzles/2024/Day07.fsx
+++ Puzzles/2024/Day07.fsx
@@ -1,9 +1,11 @@
 #time "on"
 #load "../../Tools.fs"
+#r "nuget: NFluent"
 
 open System
 open System.IO
 open AdventOfCode
+open NFluent
 
 let getInput name = 
     File.ReadAllLines(getInputPath2024 name)
@@ -13,7 +15,6 @@ let getInput name =
     )
 
 let couldBeTrue expected (numbers:int64 list) =
-    
     let rec couldBeTrueRec current remainingNumbers =
         match remainingNumbers with
         | [] -> current = expected
@@ -29,8 +30,46 @@ let couldBeTrue expected (numbers:int64 list) =
 
 let solve1 input =
     getInput input
-    |> Seq.filter (fun (exp, nums) -> couldBeTrue exp nums) |> Seq.toArray |> Dump
+    |> Seq.filter (fun (exp, nums) -> couldBeTrue exp nums) // |> Seq.toArray |> Dump
     |> Seq.map fst
     |> Seq.sum
 
 solve1 "Day07.txt"
+
+let (||||) (a:int64) (b:int64) =
+    let mutable pow = 1L
+    while pow <= b do pow <- pow * 10L
+    a * pow + b
+
+Check.That(1234 |||| 567).Equals(1234567)
+Check.That(1000 |||| 100).Equals(1000100)
+Check.That(1 |||| 56).Equals(156)
+Check.That(56 |||| 1).Equals(561)
+
+let couldBeTrueWithConcat expected (numbers:int64 list) =
+    let rec couldBeTrueWithConcatRec current remainingNumbers =
+        // Skip if we're already past the target
+        if current > expected then
+            false
+        else
+            match remainingNumbers with
+            | [] -> current = expected
+            | h::t -> 
+                if couldBeTrueWithConcatRec (current + h) t then
+                    true
+                else if couldBeTrueWithConcatRec (current * h) t then
+                    true
+                else if couldBeTrueWithConcatRec (current |||| h) t then
+                    true
+                else
+                    false
+
+    couldBeTrueWithConcatRec numbers.Head numbers.Tail
+
+let solve2 input =
+    getInput input
+    |> Seq.filter (fun (exp, nums) -> couldBeTrueWithConcat exp nums) // |> Seq.toArray |> Dump
+    |> Seq.map fst
+    |> Seq.sum
+
+solve2 "Day07.txt"
