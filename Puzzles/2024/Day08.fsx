0717a90c4db6807a2835ed0f849c0d01be35b5d3
Julien Adam <julien.adam@arcodia.fr>
2024-12-08 11:35:09 +0100

    Finished Day 8 part 2

--- Puzzles/2024/Day08.fsx
+++ Puzzles/2024/Day08.fsx
@@ -7,31 +7,59 @@ open System.IO
 open AdventOfCode
 
 let getInput name = 
-    File.ReadAllLines(getInputPath2024 name)
-    |> array2D
-
-let findAntinodes (r1,c1) (r2,c2) =
-    let dr = r2-r1
-    let dc = c2-c1
-    [(r1-dr, c1-dc); (r2+dr, c2+dc)]
-
-let solve1 input =
-    let grid = getInput input
+    let grid = File.ReadAllLines(getInputPath2024 name) |> array2D
     let antennas = 
         grid
         |> Array2DTools.enumArray2d
         |> Seq.choose (fun (r,c,v) -> match v with | '.' -> None | x -> Some (r,c,x)) // keep only antenna cells
         |> Seq.groupBy (fun (_,_,power) -> power) // group by power
         |> Seq.map (fun (power,antennas) -> power, antennas |> Seq.map (fun (r,c,_) -> (r,c)) |> Seq.toArray) // index by power, store (r,c) coords
+    grid, antennas
 
+
+let findAntinodes grid (r1,c1) (r2,c2) =
+    let dr = r2-r1
+    let dc = c2-c1
+    [(r1-dr, c1-dc); (r2+dr, c2+dc)] 
+    |> Seq.filter (fun (r,c) -> grid |> Array2DTools.isInBounds r c)
+
+let applyAntiNodeFinder finder antennas =
     antennas 
     |> Seq.collect (fun (_, antennas) ->
         Seq.allPairs antennas antennas // get all pairs of antennas of the same power
         |> Seq.filter (fun (p1,p2) -> p1 <> p2) // remove self pairs
-        |> Seq.collect (fun (p1,p2) -> findAntinodes p1 p2) // find their antinodes
-        |> Seq.filter (fun (r,c) -> grid |> Array2DTools.isInBounds r c) // filter out of bounds antinodes
+        |> Seq.collect (fun (p1,p2) -> finder p1 p2) // find their antinodes
     )
-    |> Seq.distinct // |> Seq.toArray |> Dump
-    |> Seq.length
+    |> Seq.distinct
+
+let solve1 input =
+    let grid, antennas = getInput input
+    antennas |> applyAntiNodeFinder (findAntinodes grid) |> Seq.length
 
 solve1 "Day08.txt"
+
+let findResonantHarmonicAntinodes grid (r1,c1) (r2,c2) =
+    let dr = r2-r1
+    let dc = c2-c1
+    let mutable mr1 = r1
+    let mutable mc1 = c1
+    let mutable mr2 = r2
+    let mutable mc2 = c2
+    seq {
+        // walk in one direction until out of bounds
+        while(grid |> Array2DTools.isInBounds mr1 mc1) do
+            yield (mr1,mc1)
+            mr1 <- mr1-dr
+            mc1 <- mc1-dc
+        // and in the other direction
+        while(grid |> Array2DTools.isInBounds mr2 mc2) do
+            yield (mr2,mc2)
+            mr2 <- mr2-dr
+            mc2 <- mc2-dc
+    }
+
+let solve2 input =
+    let grid, antennas = getInput input
+    antennas |> applyAntiNodeFinder (findResonantHarmonicAntinodes grid) |> Seq.length
+
+solve2 "Day08.txt"
