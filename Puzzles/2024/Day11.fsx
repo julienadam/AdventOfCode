01e6127e2ed9a390c086f109d6f3c875ee36f62b
Julien Adam <julien.adam@arcodia.fr>
2024-12-11 17:59:34 +0100

    Finished part 2
    Surprisingly, worked the first time.
    
    Used a simple memo pattern to store intermediate results.
    
    Going down the tree one stone at a time until it reaches the required
    number of steps and then back up the tree, using the memoized values to
    speed up the runtimle considerably.
    
    about 160ms, probably could be optimized further but good enough for now

--- Puzzles/2024/Day11.fsx
+++ Puzzles/2024/Day11.fsx
@@ -5,6 +5,7 @@
 open System
 open System.IO
 open AdventOfCode
+open Checked
 
 let getInput name = 
     File.ReadAllText(getInputPath2024 name) 
@@ -29,26 +30,50 @@ let (||||) (a:int64) (b:int64) : int64=
         while pow <= b do pow <- pow * 10L
         a * pow + b
 
-let blink stones =
-    stones |> Seq.collect (fun s ->
-        match s with
-        | 0L -> seq { yield 1L }
-        | x -> 
-            let ds = digits x |> Seq.toArray
-            if ds.Length % 2 = 0 then
-                ds
-                |> Seq.splitInto 2
-                |> Seq.map (fun splitDs -> splitDs |> Seq.reduce (fun a b -> a |||| b))
-            else
-                seq { yield x * 2024L }
-        )
+let blinkSingle s =
+    match s with
+    | 0L -> seq { yield 1L }
+    | x -> 
+        let ds = digits x |> Seq.toArray
+        if ds.Length % 2 = 0 then
+            ds
+            |> Seq.splitInto 2
+            |> Seq.map (fun splitDs -> splitDs |> Seq.reduce (fun a b -> a |||| b))
+        else
+            seq { yield x * 2024L }
+
+let memoizedBlinkAll initialStones totalSteps =
+    let memo = new System.Collections.Generic.Dictionary<(int*int64), int64>()
+
+    let rec blink2 stone step =
+        match step with
+        | x when x = totalSteps -> 
+            memo.TryAdd((totalSteps, stone), 1L) |> ignore
+            1L
+        | x when x > totalSteps -> failwith "should not happen"
+        | _ ->
+            match memo.TryGetValue((step, stone)) with
+            | (true, memoized) -> 
+                memoized
+            | (false, _) -> 
+                let nextStones = blinkSingle stone
+                let r = nextStones |> Seq.sumBy (fun ns -> blink2 ns (step + 1))
+                memo.Add((step, stone), r)
+                r
+
+    let result = initialStones |> Seq.sumBy(fun s -> blink2 s 0)
+    printfn "memoized a total %i intermediate results " memo.Count
+    result
+
 
 let solve1 input =
-    let mutable stones = getInput input // |> Dump
-    for x = 1 to 25 do
-        // printfn "Blink #%i" x
-        stones <- (blink stones) |> Seq.toArray // |> Dump
-
-    stones |> Seq.length
+    let mutable stones = getInput input
+    memoizedBlinkAll stones 25
 
 solve1 "Day11.txt"
+
+let solve2 input =
+    let mutable stones = getInput input
+    memoizedBlinkAll stones 75
+
+solve2 "Day11.txt"
