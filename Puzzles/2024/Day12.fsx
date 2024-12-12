bc85db60c7ac9cd60023f1a6c59a144903ecb269
Julien Adam <julien.adam@arcodia.fr>
2024-12-12 18:33:54 +0100

    Day 12 part 1 done

--- Puzzles/2024/Day12.fsx
+++ Puzzles/2024/Day12.fsx
@@ -1,3 +1,5 @@
+open System.Collections.Generic
+
 #time "on"
 #load "../../Tools.fs"
 #load "../../Tools/Array2DTools.fs"
@@ -7,6 +9,7 @@ open System
 open System.IO
 open AdventOfCode
 open Array2DTools
+open System.Drawing
 
 let getInput name = File.ReadAllLines(getInputPath2024 name) |> array2D
 
@@ -21,10 +24,8 @@ type Region = {
 
 type Cell = { plant: char; regionId: int option }
 
-open System.Drawing
-
 let printMapColor (cells:Cell[,]) =
-    let r = new Random(42);
+    let r = new Random();
     let colorMap = 
         cells 
         |> Array2DTools.enumArray2d 
@@ -44,68 +45,41 @@ let printMapColor (cells:Cell[,]) =
 let solve1 input =
     let regions = new System.Collections.Generic.Dictionary<int, Region>();
     let map = getInput input |> Array2D.map (fun v -> { plant = v; regionId = None})
-    map
-    |> Array2D.iteri (fun r c v ->
-        let neighboringPlots = 
-            map
-            |> Array2DTools.getAdjacent r c 
-            |> Seq.filter (fun (_,_,av) -> av.plant = v.plant)
-        
-        let region = 
-            match neighboringPlots |> Seq.tryPick (fun (ar,rc,av) -> av.regionId) with
-            | Some regId -> regions[regId]
-            | None ->
-                idGenerator <- idGenerator + 1
-                let region = { id = idGenerator; area = 0; perimeter = 0; plant = v.plant }
-                regions.Add(region.id, region)
-                region
+    let otherPlots = new HashSet<int*int>()
+    
+    let rec walkRec (r,c) =
+        otherPlots.Remove(r,c) |> ignore
+        let cell = map[r,c]
+        match cell.regionId with
+        | None -> 
+            let neighbors = 
+                map
+                |> Array2DTools.getAdjacent r c  |> Seq.toArray
+            let otherNeighbors = neighbors |> Seq.filter (fun (_,_,av) -> av.plant <> cell.plant)
+            otherNeighbors |> Seq.iter (fun (ar,ac,_) -> otherPlots.Add(ar,ac) |> ignore)
+            let neighboringPlots = neighbors |> Seq.filter (fun (_,_,av) -> av.plant = cell.plant)
+            let region = 
+                match neighboringPlots |> Seq.tryPick (fun (ar,rc,av) -> av.regionId) with
+                | Some regId -> regions[regId]
+                | None ->
+                    idGenerator <- idGenerator + 1
+                    let region = { id = idGenerator; area = 0; perimeter = 0; plant = cell.plant }
+                    regions.Add(region.id, region)
+                    region
+            region.area <- region.area + 1L
+            region.perimeter <- region.perimeter + (4L - (neighboringPlots |> Seq.length |> int64))
+            Array2D.set map r c { cell with regionId = Some region.id }
+            neighboringPlots |> Seq.iter(fun (ar,ac,_) -> walkRec (ar,ac))
+        | Some _ -> ()
+     
+    otherPlots.Add(0,0) |> ignore
+    while otherPlots.Count <> 0 do
+        walkRec(otherPlots |> Seq.head)
 
-        region.area <- region.area + 1L
-        region.perimeter <- region.perimeter + (4L - (neighboringPlots |> Seq.length |> int64))
-        Array2D.set map r c { v with regionId = Some region.id }
-    )
+    // printMapColor map
 
-    printMapColor map
-
-    regions 
-    |> Seq.iter (fun kvp -> 
-        printfn "Region %i of type %c area %i perimeter %i and price %i" kvp.Key kvp.Value.plant kvp.Value.area kvp.Value.perimeter (kvp.Value.area * kvp.Value.perimeter))
-    // |> Seq.sumBy (fun kvp -> kvp.Value.perimeter * kvp.Value.area)
+    regions  //|> Seq.iter (fun kvp -> printfn "Region %i of type %c area %i perimeter %i and price %i" kvp.Key kvp.Value.plant kvp.Value.area kvp.Value.perimeter (kvp.Value.area * kvp.Value.perimeter))
+    |> Seq.sumBy (fun kvp -> kvp.Value.perimeter * kvp.Value.area)
 
 
 solve1 "Day12_sample2.txt"
-
-// cell
-  // plant type i.e. character
-  // region id option, if no region, it was not checked yet
-
-// region
-  // area
-  // perimeter
-
-//match v.regionId with
-     //| Some reg -> 
-     //    let region = regions[reg]
-     //    // TODO : handle case where another region is next to this one ?
-     //    let neighboringPlots = 
-     //        map 
-     //        |> Array2DTools.getAdjacent r c 
-     //        |> Seq.filter (fun (_,_,av) -> av.plant = v.plant)
-     //        |> Seq.length
-     //    region.area <- region.area + 1L
-     //    region.perimeter <- region.perimeter + (neighboringPlots |> int64)
-     //| None -> 
-     //    let id = Guid.NewGuid()
-     //    let region = { area = 0; perimeter = 0; plant = v.plant; id = id}
-     //    regions.Add(id,region) |> ignore
-         
-
-         
-         //let mutable r1 = { area = 0; perimeter = 0; plant = 'C' }
-         
-         //let cA = { plant = 'C'; region = ref r1}
-         
-         //cA.region.contents.area <- 1
-         
-         //r1 |> Dump
-         //cA |> Dump
