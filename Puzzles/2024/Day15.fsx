29c3f209ff282a5188d5fc4c909ccf86d30a88a8
Julien Adam <julien.adam@arcodia.fr>
2024-12-15 11:54:17 +0100

    Finished Day 15

--- Puzzles/2024/Day15.fsx
+++ Puzzles/2024/Day15.fsx
@@ -11,7 +11,7 @@ open NFluent
 
 let getInput name = 
     let gridRaw,movesRaw = File.ReadAllText(getInputPath2024 name).Replace("\r", "") |> ssplit "\n\n" |> tupleize2
-    gridRaw |> splitLines |> array2D, movesRaw.Replace("\r", "").Replace("\n", "")
+    gridRaw |> splitLines, movesRaw.Replace("\r", "").Replace("\n", "")
 
 let mapMoves = 
     function
@@ -52,6 +52,7 @@ let gpsCoords r c = r*100+c
 
 let solve1 input =
     let grid, moves = getInput input
+    let grid = grid |> array2D
     let (rr,rc,_) =  grid |> Array2DTools.filteri (fun _ _ v -> v = '@') |> Seq.head
 
     let mutable r = rr
@@ -73,3 +74,86 @@ Check.That(solve1 "Day15_sample1.txt").Equals(10092)
 Check.That(solve1 "Day15_sample2.txt").Equals(2028)
 
 solve1 "Day15.txt"
+
+let expandWarehouse (input:string) = input.Replace("#", "##").Replace(".", "..").Replace("O", "[]").Replace("@", "@.")
+
+/// try to move in the direction provided
+/// returns the move if it's possible, or an empty list
+/// it's recusive so if it finds a box in its way it will try to move it
+/// and this box will also try to move etc.
+// the final result is a list of moves triggered by the initial move
+// handles the larger boxes
+let rec tryMove2 (r,c) (grid:char[,]) moveFunc move =
+    //match m with
+    //| '<' -> tryMove (r,c) grid Array2DTools.tryGetLeft
+    //| '>' -> tryMove (r,c) grid Array2DTools.tryGetRight
+    //| '^' -> 
+    //| 'v' -> 
+    
+    match grid |> moveFunc r c with
+    | Some (ar, ac, av) when av = '.' -> [(r,c),(ar,ac)]
+    | Some (ar, ac, av) when av = ']' -> 
+        match move with 
+        | '^'
+        | 'v' ->
+            match (tryMove2 (ar,ac) grid moveFunc move), (tryMove2 (ar,ac-1) grid moveFunc move) with
+            | [],_
+            | _,[] -> []
+            | (half1,half2) -> ((r,c),(ar,ac))::(List.concat [half1; half2])
+        | _ ->
+            match tryMove2 (ar,ac) grid moveFunc move with
+            | [] -> []
+            | x -> ((r,c),(ar,ac))::x
+
+    | Some (ar, ac, av) when av = '[' -> 
+        match move with 
+        | '^'
+        | 'v' ->
+            match (tryMove2 (ar,ac) grid moveFunc move), (tryMove2 (ar,ac+1) grid moveFunc move) with
+            | [],_
+            | _,[] -> []
+            | (half1,half2) -> ((r,c),(ar,ac))::(List.concat [half1; half2])
+        | _ ->
+            match tryMove2 (ar,ac) grid moveFunc move with
+            | [] -> []
+            | x -> ((r,c),(ar,ac))::x
+    | Some (_, _, av) when av = '#' -> []
+    | None -> []
+    | x -> failwithf "Invalid cell contents %A" x
+
+/// applies all the moves in reverse order and returns the
+/// final position of the robot, removes duplicate moves
+/// caused by the larger boxes
+let applyMoves2 (moves:((int*int)*(int*int))seq) (grid:char[,])=
+    moves 
+    |> Seq.rev
+    |> Seq.distinct
+    |> Seq.iter (fun ((sr,sc),(er,ec)) ->
+        grid[er,ec] <- grid[sr,sc]
+        grid[sr,sc] <- '.')
+
+    moves |> Seq.tryHead |> Option.map snd
+
+
+let solve2 input =
+    let grid, moves = getInput input
+    let grid = grid |> Seq.map expandWarehouse |> array2D
+    let (rr,rc,_) =  grid |> Array2DTools.filteri (fun _ _ v -> v = '@') |> Seq.head
+
+    let mutable r = rr
+    let mutable c = rc
+
+    for m in moves do
+        match applyMoves2 (tryMove2 (r,c) grid (m |> mapMoves) m) grid with
+        | Some (nr,nc) -> r <- nr; c <- nc
+        | None -> ()
+
+    // Calculate gps coords for all boxes
+    grid 
+    |> filteri (fun _ _ v -> v = '[')
+    |> Seq.map (fun (r,c,_) -> gpsCoords r c)
+    |> Seq.sum
+
+Check.That(solve2 "Day15_sample1.txt").Equals(9021)
+
+solve2 "Day15_sample1.txt"
