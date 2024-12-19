ccb27607b0180d616d8f23d0ef7303f7413a39c0
Julien Adam <julien.adam@arcodia.fr>
2024-12-19 16:55:37 +0100

    Improved Day 19 performance
    Around 30ms now, using PSeq and Memory<char>
    
    Maybe putting the memo in common between all designs could help cut down
    more but it would mean keeping strings instead of positions or having
    some sort of map for positions between designs ... maybe not worth it
    considering only very small portions could be reused (the last common
    letters)
    
    Added a benchmark.net test run for fun. Memory<char> blows away string
    by a mile. 10MB allocations total vs 650MB. And that probably all in the
    memo.

--- Program.fs
+++ Program.fs
@@ -1,5 +1,107 @@
 ﻿open System
 open System.IO
 open AdventOfCode
+open System.Collections.Generic
+open FSharp.Collections.ParallelSeq
+open Checked
 
-printfn "Hello"
+let getInput name = 
+    let patterns, designs = 
+        File.ReadAllText(getInputPath2024 name).Replace("\r", "")
+        |> ssplitNoEmpty "\n\n" |> tupleize2
+    patterns |> ssplitNoEmpty ", ", designs |> ssplitNoEmpty "\n"
+
+let solveCore (patterns:string array) (designs:string array) =
+    let countArrangements design =
+        let memo = new Dictionary<int, int64>()
+        let rec countArrangementsRec index (design:Memory<char>) =
+            if index = design.Length then 1L
+            else
+                let matchingPatterns = patterns |> Seq.filter (fun p -> design.Slice(index).Span.StartsWith(p))
+                if matchingPatterns |> Seq.isEmpty then 
+                    memo.TryAdd(index, 0L) |> ignore
+                    0L
+                else
+                    // use the memoized solution if it exists, or compute and memoize if not
+                    match memo.TryGetValue(index) with
+                    | true, memoized -> memoized
+                    | false, _ ->
+                        let sum = 
+                            matchingPatterns
+                            |> Seq.map (fun p -> countArrangementsRec (index + p.Length) design)
+                            |> Seq.sum
+                        memo.TryAdd(index, sum) |> ignore
+                        sum
+        countArrangementsRec 0 design
+
+    designs |> Seq.sumBy (fun d -> countArrangements (new Memory<char>(d.ToCharArray())))
+
+
+let solveCoreString (patterns:string array) (designs:string array) =
+    let countArrangements design =
+        let memo = new Dictionary<int, int64>()
+        let rec countArrangementsRec index (design:string)  =
+            if index = design.Length then 1L
+            else
+                let matchingPatterns = patterns |> Seq.filter (fun p -> design.Substring(index).StartsWith(p))
+                if matchingPatterns |> Seq.isEmpty then 
+                    memo.TryAdd(index, 0L) |> ignore
+                    0L
+                else
+                    // use the memoized solution if it exists, or compute and memoize if not
+                    match memo.TryGetValue(index) with
+                    | true, memoized -> memoized
+                    | false, _ ->
+                        let sum = 
+                            matchingPatterns
+                            |> Seq.map (fun p -> countArrangementsRec (index + p.Length) design)
+                            |> Seq.sum
+                        memo.TryAdd(index, sum) |> ignore
+                        sum
+        countArrangementsRec 0 design
+
+    designs |> Seq.sumBy (fun d -> countArrangements d)
+
+let solveCoreParallel (patterns:string array) (designs:string array) =
+    let countArrangements design =
+        let memo = new Dictionary<int, int64>()
+        let rec countArrangementsRec index (design:Memory<char>) =
+            if index = design.Length then 1L
+            else
+                let matchingPatterns = patterns |> Seq.filter (fun p -> design.Slice(index).Span.StartsWith(p))
+                if matchingPatterns |> Seq.isEmpty then 
+                    memo.TryAdd(index, 0L) |> ignore
+                    0L
+                else
+                    // use the memoized solution if it exists, or compute and memoize if not
+                    match memo.TryGetValue(index) with
+                    | true, memoized -> memoized
+                    | false, _ ->
+                        let sum = 
+                            matchingPatterns
+                            |> Seq.map (fun p -> countArrangementsRec (index + p.Length) design)
+                            |> Seq.sum
+                        memo.TryAdd(index, sum) |> ignore
+                        sum
+        countArrangementsRec 0 design
+
+    designs |> PSeq.sumBy (fun d -> countArrangements (new Memory<char>(d.ToCharArray())))
+
+open BenchmarkDotNet.Running
+open BenchmarkDotNet.Attributes
+
+[<ShortRunJob>]
+[<MemoryDiagnoser>]
+type public Bench () =
+    let patterns, designs = getInput "Day19.txt"
+
+    [<Benchmark>]
+    member public this.SolveWithString() = solveCoreString patterns designs
+
+    [<Benchmark>]
+    member public this.SolveWithMemory() = solveCore patterns designs
+
+    [<Benchmark>]
+    member public this.SolveWithMemoryParallel() = solveCoreParallel patterns designs
+
+BenchmarkRunner.Run<Bench>() |> ignore
--- Puzzles/2024/Day19.fsx
+++ Puzzles/2024/Day19.fsx
@@ -5,8 +5,8 @@ open System
 open System.IO
 open AdventOfCode
 open System.Text.RegularExpressions
-open Checked
 
+open Checked
 let getInput name = 
     let patterns, designs = 
         File.ReadAllText(getInputPath2024 name).Replace("\r", "")
@@ -17,22 +17,25 @@ let solve1 input =
     let patterns, designs = getInput input
     let reTowels = sprintf("^(%s)+$") (String.Join("|", patterns))
     let regex = new Regex(reTowels, RegexOptions.Compiled)
-    designs |> Seq.filter (fun d -> regex.IsMatch(d)) |> Seq.length
+    designs |> Seq.filter regex.IsMatch |> Seq.length
 
 solve1 "Day19_sample1.txt"
 solve1 "Day19.txt"
 
+#r "nuget: FSharp.Collections.ParallelSeq"
+open FSharp.Collections.ParallelSeq
 open System.Collections.Generic
 
-let solve2 input =
-    let patterns, designs = getInput input
+let solveCoreParallel (patterns:string array) (designs:string array) =
     let countArrangements design =
         let memo = new Dictionary<int, int64>()
         let rec countArrangementsRec index (design:Memory<char>) =
             if index = design.Length then 1L
             else
                 let matchingPatterns = patterns |> Seq.filter (fun p -> design.Slice(index).Span.StartsWith(p))
-                if matchingPatterns |> Seq.isEmpty then 0L
+                if matchingPatterns |> Seq.isEmpty then 
+                    memo.TryAdd(index, 0L) |> ignore
+                    0L
                 else
                     // use the memoized solution if it exists, or compute and memoize if not
                     match memo.TryGetValue(index) with
@@ -46,7 +49,11 @@ let solve2 input =
                         sum
         countArrangementsRec 0 design
 
-    designs |> Seq.sumBy (fun d -> countArrangements (new Memory<char>(d.ToCharArray())))
+    designs |> PSeq.sumBy (fun d -> countArrangements (new Memory<char>(d.ToCharArray())))
+
+let solve2 input =
+    let patterns, designs = getInput input
+    solveCoreParallel patterns designs
 
 solve2 "Day19_sample1.txt"
 solve2 "Day19.txt"
