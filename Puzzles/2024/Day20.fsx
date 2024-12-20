32312a13603253ffee5aed3e5efdcc6c38b8f54d
Julien Adam <julien.adam@gmail.com>
2024-12-20 19:00:28 +0100

    Day 20 simpler and faster impl
    finally realized that it is not a maze, but a single track. Simplified
    the path finding to a traversal without bounds checking.
    
    finding the cheats is a lot simpler

--- Puzzles/2024/Day20.fsx
+++ Puzzles/2024/Day20.fsx
@@ -1,45 +1,54 @@
+open System.Threading
 
 #time "on"
-#r "nuget: optimizedpriorityqueue"
 #load "../../Tools.fs"
 #load "../../Tools/Array2DTools.fs"
-#load "../../Tools/Dijkstra.fs"
 #load "../../Tools/Distance.fs"
 #r "nuget: FSharp.Collections.ParallelSeq"
 
 open System.IO
 open AdventOfCode
-open Array2DTools
 open Checked
+open System.Collections.Generic
 open FSharp.Collections.ParallelSeq
 
 let getInput name = File.ReadAllLines(getInputPath2024 name) |> array2D
 
-let solveDij maxPicoSecondsAllowed input cutOff = 
+let solve cheatDuration input minCheat = 
     let grid = getInput input 
-    let one _ _ = 1f
+    let (sr,sc, _) = grid |> Array2DTools.findi (fun _ _ v -> v = 'S')
     let (er,ec, _) = grid |> Array2DTools.findi (fun _ _ v -> v = 'E')
-    let vertices = grid |> filteri (fun _ _ v -> v <> '#') |> Seq.map (fun (r,c,_) -> (r,c)) |> Seq.toList
-    let getNeighbors (r,c) = grid |> Array2DTools.getAdjacent r c |> Seq.filter (fun (_,_,v) -> v <> '#') |> Seq.map (fun (r,c,_) -> (r,c)) |> Seq.toList
 
-    let distMatrix = Dijkstra.getDistMatrix (er,ec) vertices getNeighbors one
-    let sortedByDist = distMatrix |> Seq.map (fun kvp -> kvp.Key, kvp.Value) |> Seq.sortBy snd |> Seq.toArray
+    // it's not a maze, so there's only a single path
+    // walk it backwards so we have a list of points and the index is the distance to the goal
+    let path = new List<int*int>()
+    let dirs = [(0,1);(0,-1);(1,0);(-1,0)]
+    let (++) (r1,c1) (r2,c2) = (r1+r2), (c1+c2)
+    let rec walk pos prev =
+        path.Add(pos)
+        if pos = (sr,sc) then ()
+        else
+            let next = dirs |> Seq.map(fun d -> pos++d) |> Seq.find (fun (r,c) -> grid[r,c] <> '#' && (r,c) <> prev)
+            walk next pos
 
-    sortedByDist
-    |> PSeq.mapi (fun i (a,d1) ->
-        sortedByDist 
-        |> Array.skip i 
-        |> Seq.filter (fun (b,d2) ->
-            let md = (manhattanDistPoints a b |> float32)
-            d2 - d1 - md >= cutOff && md <= maxPicoSecondsAllowed)
-        |> Seq.length
+    walk (er,ec) (er,ec)
+
+    let cheats = ref 0
+
+    path |> PSeq.iteri (fun index a ->
+        // Find shortcuts (they are at least more than minCheat cells away)
+        for cheatTo = index + minCheat to path.Count - 1 do 
+            let b = path[cheatTo]
+            let md = manhattanDistPoints a b
+            if cheatTo - index - md >= minCheat && md <= cheatDuration then
+                Interlocked.Increment(cheats) |> ignore
     )
-    |> Seq.sum
+    cheats.Value
 
-let solve1 = solveDij 2f
-// solve1 "Day20_sample1.txt" 12f
-solve1 "Day20.txt" 100f
+let solve1 = solve 2
 
-let solve2 = solveDij 20f
-// solveDij "Day20_sample1.txt" 50f 20f
-solve2 "Day20.txt" 100f 
+solve1 "Day20.txt" 100
+
+let solve2 = solve 20
+
+solve2 "Day20.txt" 100
