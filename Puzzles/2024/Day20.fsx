9db81e9072877c301717654c65e3c653fcf6f532
Julien Adam <julien.adam@gmail.com>
2024-12-20 17:09:27 +0100

    Finished Day 20
    Very slow O(n²) lots of room for improvement but finds the answer in
    under 6 seconds for now

--- Puzzles/2024/Day20.fsx
+++ Puzzles/2024/Day20.fsx
@@ -1,30 +1,37 @@
 #time "on"
+#r "nuget: optimizedpriorityqueue"
 #load "../../Tools.fs"
 #load "../../Tools/Array2DTools.fs"
-#load "../../Tools/AStar.fs"
+#load "../../Tools/Dijkstra.fs"
 #load "../../Tools/Distance.fs"
 
 open System
 open System.IO
 open AdventOfCode
+open Array2DTools
 open Checked
-
 let getInput name = File.ReadAllLines(getInputPath2024 name) |> array2D
 
-let solve1 input =
+let solveDij maxPicoSecondsAllowed input cutOff = 
     let grid = getInput input 
-    let (sr,sc, _) = grid |> Array2DTools.findi (fun _ _ v -> v = 'S')
     let (er,ec, _) = grid |> Array2DTools.findi (fun _ _ v -> v = 'E')
-    
-    let config:FullAStar.Config<(int*int)> = {
-        gCost = (fun _ _ -> 1.0)
-        fCost = (fun a b -> manhattanDistPoints a b |> float)
-        neighbors = (fun (r,c) -> grid |> Array2DTools.getAdjacent r c |> Seq.filter (fun (_,_,v) -> v <> '#') |> Seq.map (fun (r,c,_) -> (r,c)))
-        maxIterations = None
-    }
+    let vertices = grid |> filteri (fun _ _ v -> v <> '#') |> Seq.map (fun (r,c,_) -> (r,c)) |> Seq.toList
+    let getNeighbors (r,c) = grid |> Array2DTools.getAdjacent r c |> Seq.filter (fun (_,_,v) -> v <> '#') |> Seq.map (fun (r,c,_) -> (r,c)) |> Seq.toList
 
-    match FullAStar.search (sr,sc) (er,ec) config with
-    | Some p -> printfn "Best path is %i long" ((p |> Seq.length) - 1)
-    | None -> printfn "No path found"
+    let distMatrix = Dijkstra.getDistMatrix (er,ec) vertices getNeighbors (fun a b -> 1.0f)
 
-solve1 "Day20_sample1.txt"
+    let matrix = distMatrix |> Seq.map (fun kvp -> kvp.Key, kvp.Value) |> Seq.toList
+    Seq.allPairs matrix matrix 
+    |> Seq.filter (fun ((a,d1), (b,d2)) -> 
+        let md = (manhattanDistPoints a b |> float32)
+        d2 - d1 - md >= cutOff && md <= maxPicoSecondsAllowed)
+    |> Seq.length
+
+
+let solve1 = solveDij 2f
+// solve1 "Day20_sample1.txt" 12f
+solve1 "Day20.txt" 100f
+
+let solve2 = solveDij 20f
+// solveDij "Day20_sample1.txt" 50f 20f
+solve2 "Day20.txt" 100f 
