32908d6f0688ff6a1fd10856f0d2cf953b20b997
Julien Adam <julien.adam@arcodia.fr>
2024-12-22 15:39:14 +0100

    Day 21 done
    so many dead ends and mistakes on this one !

--- Puzzles/2024/Day21.fsx
+++ Puzzles/2024/Day21.fsx
@@ -1,3 +1,5 @@
+open System.Collections.Generic
+
 #time "on"
 #load "../../Tools.fs"
 #load "../../Tools/SeqEx.fs"
@@ -8,6 +10,8 @@ open System.IO
 open AdventOfCode
 open Array2DTools
 open Checked
+
+let getInput name = File.ReadAllLines(getInputPath2024 name)
 
 let manhattanPath (ra,ca) (rb,cb) = seq {
     if rb > ra then
@@ -24,398 +28,104 @@ let allManhattanPaths pA pB =
     let dirs = manhattanPath pA pB |> Seq.toList
     SeqEx.permutations dirs.Length dirs |> Seq.distinct
 
+let numPadMap = 
+    [ 
+        "789"; 
+        "456"; 
+        "123"; 
+        ".0A"
+    ] |> array2D |> filteri (fun _ _ v -> v <> '.') |> Seq.map (fun (r,c,v) -> v, (r,c)) |> Map.ofSeq
 
-let numPad = [
-    "789"
-    "456"
-    "321"
-    ".0A"
-]
+let dirMap = [('^', (-1,0)); ('v', (1,0)); ('>', (0,1)); ('<', (0,-1))] |> Map.ofSeq
+let (++) (r1,c1) (r2,c2) = (r1+r2, c1+c2)
 
-let numPadMap = numPad |> array2D |> filteri (fun _ _ v -> v <> '.') |> Seq.map (fun (r,c,v) -> v, (r,c)) |> Map.ofSeq
+// Check that we don't go into the forbidden spot along the way
+let hitsForbiddenSpot (start:int*int) forbidden (instructions:char seq) =
+    let mutable spot = start
+    instructions |> Seq.exists (fun i ->
+        spot <- spot ++ dirMap[i]
+        spot = forbidden
+    )
 
 let allNumPadPaths a b =
-    let pA = numPadMap[a] // TODO: Could be constants for a little perf boost
+    let pA = numPadMap[a]
     let pB = numPadMap[b]
-    let result = allManhattanPaths pA pB |> Seq.toList
-    // Filter out paths that would go through the empty space left of 0
-    if pA = (3,1) then // '0'
-        result |> List.filter (fun l -> match l with | '<'::_ -> false | _ -> true)
-    else if pA = (3,2) then // 'A'
-        result |> List.filter (fun l -> match l with | '<'::'<'::_ -> false | _ -> true)
-    else if pA = (2,0) then // '3'
-        result |> List.filter (fun l -> match l with | 'v'::_ -> false | _ -> true)
-    else if pA = (1,0) then // '4'
-        result |> List.filter (fun l -> match l with | 'v'::'v'::_ -> false | _ -> true)
-    else if pA = (0,0) then // '7'
-        result |> List.filter (fun l -> match l with | 'v'::'v'::'v'::_ -> false | _ -> true)
-    else
-        result
+    allManhattanPaths pA pB
+    |> Seq.filter (fun instructions -> not(hitsForbiddenSpot pA (3,0) instructions))
+    |> Seq.toList
 
-let dirPad = [
-    ".^A"
-    "<v>"
-]
+// allNumPadPaths '0' '1'
 
-let dirPadMap = dirPad |> array2D |> filteri (fun _ _ v -> v <> '.') |> Seq.map (fun (r,c,v) -> v, (r,c)) |> Map.ofSeq
+let dirPadMap = [ ".^A"; "<v>" ] |> array2D |> filteri (fun _ _ v -> v <> '.') |> Seq.map (fun (r,c,v) -> v, (r,c)) |> Map.ofSeq
 
 let allDirPadPaths a b =
-    let pA = dirPadMap[a] // TODO: Could be constants for a little perf boost
+    let pA = dirPadMap[a]
     let pB = dirPadMap[b]
-    let result = allManhattanPaths pA pB |> Seq.toList
-    // Filter out paths that would go through the empty space left of 0
-    if pA = (0,1) then // '^'
-        result |> List.filter (fun l -> match l with | '<'::_ -> false | _ -> true)
-    else if pA = (0,2) then // 'A'
-        result |> List.filter (fun l -> match l with | '<'::'<'::_ -> false | _ -> true)
-    else if pA = (1,0) then // '<'
-        result |> List.filter (fun l -> match l with | '^'::_ -> false | _ -> true)
-    else
-        result
+    allManhattanPaths pA pB
+    |> Seq.filter (fun instructions -> not(hitsForbiddenSpot pA (0,0) instructions))
+    |> Seq.toList
 
-let keepShortest (strings: string list) =
-    let minLength = (strings |> List.minBy (fun s -> s.Length)).Length
-    strings |> List.filter(fun s -> s.Length = minLength)
+// allDirPadPaths '<' 'A'
 
-let vacuumToNumPad (input:string) =
-    let rec vacuumToNumPadRec prev code commands =
-        match code with
-        | [] -> commands
-        | key::tail ->
-            commands 
-            |> List.collect(fun cmd ->
-                let allPaths = 
-                    allNumPadPaths prev key 
-                    |> List.map (fun charList -> cmd + new String(charList |> List.toArray) + "A")
-                vacuumToNumPadRec key tail allPaths
-            )
+let numPadMovesMap =
+    let npChars = "0123456789A".ToCharArray()
+    Array.allPairs npChars npChars
+    |> Seq.map (fun (a,b) -> 
+        if a = b then 
+            (a,b), ["A"]
+        else
+            (a,b), allNumPadPaths a b |> List.map (fun s -> new String(s |> List.toArray) + "A")
+    )
+    |> Map.ofSeq
 
-    vacuumToNumPadRec 'A' ((input.ToCharArray()) |> Array.toList) [""]
+let dirPadMovesMap = 
+    let dpChars = "<>^vA".ToCharArray() 
+    Array.allPairs dpChars dpChars
+    |> Seq.map (fun (a,b) -> 
+        if a = b then 
+            (a,b), ["A"]
+        else
+            (a,b), allDirPadPaths a b |> List.map (fun s -> new String(s |> List.toArray) + "A")
+    )
+    |> Map.ofSeq
 
-//vacuumToNumPad "029A" |> Seq.map (fun s -> s.Length)
+let getShortest code maxDepth =
+    let memo = new Dictionary<string*int*int, int64>()
 
-open System.Collections.Generic
+    let rec getShortestRec input depth =
+        match memo.TryGetValue((input, maxDepth, depth)) with
+        | true, r -> r
+        | false, _ ->
+            let mutable prevKey = 'A'
+            let mutable currLength = 0L
+            for key in input do
+                let movesMap = if depth = 0 then numPadMovesMap else dirPadMovesMap
+                let moves = movesMap[(prevKey, key)]
 
-let memoDir = new Dictionary<char*char, char list list>()
-
-// memoize this ?
-let dirPadToDirPad (input:string) =
-    let mutable bestLength = Int32.MaxValue
-    let paths = new List<string>()
-
-    let rec dirPadToDirPadRec prev code (command:string) =
-        match code with
-        | [] -> 
-            if command.Length < bestLength then
-                bestLength <- command.Length
-                paths.Add(command)
-            else
-                ()
-        | key::tail ->
-            let paths = 
-                match memoDir.TryGetValue((prev, key)) with
-                | true, p -> p
-                | false, _ -> 
-                    let p = allDirPadPaths prev key 
-                    memoDir.Add((prev, key), p)
-                    p
-
-            let allPaths = 
-                paths
-                |> List.map (fun charList -> command + new String(charList |> List.toArray) + "A")
-            for p in allPaths do
-                dirPadToDirPadRec key tail p
-
-    dirPadToDirPadRec 'A' ((input.ToCharArray()) |> Array.toList) ""
-    paths |> Seq.filter (fun p -> p.Length = bestLength)
-
-let p1 = 
-    vacuumToNumPad "029A"
-    |> Seq.collect dirPadToDirPad |> Seq.toList |> keepShortest
-    |> Seq.collect dirPadToDirPad |> Seq.toList |> keepShortest
-    |> Seq.collect dirPadToDirPad |> Seq.toList |> keepShortest
-
-
-//|> Seq.collect dirPadToDirPad
-//|> Seq.collect dirPadToDirPad
-//|> Seq.toArray
-
-//let p2 = p1 |> Array.sortBy (fun s -> s.Length)
-
-//p2 |> Array.map (fun s -> s.Length)
-//(p2 |> Array.last).Length
-
-//let orderedPaths = 
-//    vacuumToNumPad "029A"
-//    |> Seq.collect dirPadToDirPad
-//    |> Seq.collect dirPadToDirPad |> Seq.length |> Dump
-//    |> Seq.collect dirPadToDirPad
-//    |> Seq.head
-
-
-
-
-let getInput name = File.ReadAllLines(getInputPath2024 name)
+                if (depth = maxDepth) then
+                    currLength <- currLength + (moves[0].Length |> int64)
+                else
+                    let minLength =
+                        moves
+                        |> Seq.map(fun path -> getShortestRec path (depth + 1)) 
+                        |> Seq.min
+                    currLength <- currLength + minLength
+                prevKey <- key;
+            memo[(input, maxDepth, depth)] <- currLength;
+            currLength
+    getShortestRec code 0
 
 let solve1 input = 
-    getInput input |> Dump
+    getInput input 
+    |> Seq.map (fun code -> (code[0..2] |> int64 ), (getShortest code 2))
+    |> Seq.sumBy (fun (a,b) -> a * b)
 
-solve1 "Day21_sample1.txt"
+// solve1 "Day21_sample1.txt"
+solve1 "Day21.txt"
 
+let solve2 input = 
+    getInput input 
+    |> Seq.map (fun code -> (code[0..2] |> int64 ), (getShortest code 25))
+    |> Seq.sumBy (fun (a,b) -> a * b)
 
-
-let mapNumPadToDirPad code = 
-    ("A" + code) // Insert an extra A to account for the move from A to the first code letter
-    |> Seq.pairwise 
-    |> Seq.map (fun (a, b) -> 
-        allNumPadPaths a b 
-        |> List.map (fun charList -> sprintf "%s%c" (new String(charList |> List.toArray)) 'A')
-    )
-    |> Seq.toList
-
-mapNumPadToDirPad "029A"
-
-(*
-
-let mapNumPadToDirPad code = 
-    ("A" + code) // Insert an extra A to account for the move from A to the first code letter
-    |> Seq.pairwise 
-    |> Seq.map (fun (a,b) -> 
-        let allPathsTob = 
-            allNumPadPaths a b 
-            |> List.map (fun charList -> sprintf "%s%c" (new String(charList |> List.toArray)) 'A')
-        b, allPathsTob)
-    |> Seq.toList
-
-let possibleDepressurizedRobotInput = mapNumPadToDirPad "029A" |> Dump
-
-let mapDirPadToDirPad (dirPadCommands:string) =
-    ("A" + dirPadCommands) // Insert an extra A to account for the move from A to the first code letter
-    |> Seq.pairwise 
-    |> Seq.map (fun (a,b) -> 
-        let allPathsTob = 
-            allDirPadPaths a b 
-            |> List.map (fun charList -> sprintf "%s%c" (new String(charList |> List.toArray)) 'A')
-        b, allPathsTob)
-    |> Seq.toList
-
-let rec filterBestSequences (c,(dirPadSequences:string list)) =
-    let sorted = 
-        dirPadSequences
-        |> List.map mapDirPadToDirPad
-        |> List.map (fun irrSeqs -> irrSeqs, irrSeqs |> List.sumBy (fun s -> (s |> snd |> List.head ).Length))
-        |> List.sortBy snd
-
-    // Compute the best path length
-    let bestPathLength = sorted.Head |> snd
-    printfn "best path for %c %i" c bestPathLength
-    let bestSequences = 
-        sorted 
-        |> List.takeWhile (fun (_, score) -> score = bestPathLength) 
-        |> List.map fst
-        // |> List.map (fun x -> x |> List.map (fun y -> filterBestSequences y))
-    c, bestSequences
-
-let getBestDepressurizedRobotInputs (inputs:(char * string list) list) =  inputs |> List.map filterBestSequences
-
-let bestDepressurizedRobotInputs = getBestDepressurizedRobotInputs possibleDepressurizedRobotInput
-
-//let getPossibleFrozenRobotInputs (depressurizedRobotCommands:string) =
-//    ("A" + depressurizedRobotCommands) // Insert an extra A to account for the move from A to the first code letter
-//    |> Seq.pairwise 
-//    |> Seq.map (fun (a,b) -> 
-//        let allPathsTob = 
-//            allDirPadPaths a b 
-//            |> List.map (fun charList -> sprintf "%s%c" (new String(charList |> List.toArray)) 'A')
-//        b, allPathsTob)
-//    |> Seq.toList
-
-//let getBestFrozenRobotInputs (inputs: (char * (char * string list) list list) list) =
-//    ()
-
-[
-    ('0', 
-        [
-            [
-                ('<', ["v<<A"; "<v<A"]); 
-                ('A', [">^>A"; ">>^A"])
-            ]
-        ]
-    );
-    ('2', 
-        [
-            [
-                ('^', ["<A"]); ('A', [">A"])
-            ]
-        ]
-    );
-    ('9',
-        [
-            [
-                ('^', ["<A"]); 
-                ('^', ["A"]); 
-                ('>', ["v>A"; ">vA"]); 
-                ('A', ["^A"])
-            ]
-            [
-                ('>', ["vA"]); 
-                ('^', ["^<A"; "<^A"]); 
-                ('^', ["A"]); 
-                ('A', [">A"])
-            ]
-        ]
-    );
-    ('A',
-        [
-            [
-                ('v', ["v<A"; "<vA"]); 
-                ('v', ["A"]); 
-                ('v', ["A"]); 
-                ('A', ["^>A"; ">^A"])
-            ]
-        ]
-    )
-]
-
-// let getPossibleFrozenRobotInputs 
-[
-    ('0', 
-        [
-            [
-                ["v<<A"; "<v<A"]; [">^>A"; ">>^A"]
-            ]
-        ]); 
-    ('2', 
-        [
-            [
-                ["<A"]; [">A"]
-            ]
-        ]);
-    ('9', 
-        [
-            [
-                ["<A"]; ["A"]; ["v>A"; ">vA"]; ["^A"]
-            ]; 
-            [
-                ["vA"]; ["^<A"; "<^A"]; ["A"]; [">A"]
-            ]
-        ]
-    );
-    ('A', 
-        [
-            [
-                ["v<A"; "<vA"]; ["A"]; ["A"]; ["^>A"; ">^A"]
-            ]
-        ]
-    )
-]
-
-//[
-//    // 0
-//    [ 
-//        seq [
-//            ["v<<A"; "<v<A"]; [">^>A"; ">>^A"]
-//        ]
-//    ]; 
-//    // 2
-//    [
-//        seq [
-//            ["<A"]; [">A"]
-//        ]
-//    ];
-//    // 9
-//    [
-//        seq [
-//            ["<A"]; ["A"]; ["v>A"; ">vA"]; ["^A"]
-//        ];
-//        seq [
-//            ["<A"]; ["v>A"; ">vA"]; ["^<A"; "<^A"]; [">A"]
-//        ];
-//        seq [
-//            ["vA"]; ["^<A"; "<^A"]; ["A"]; [">A"]
-//        ]
-//    ];
-//    // A
-//    [
-//        seq [
-//            ["v<A"; "<vA"]; ["A"]; ["A"]; ["^>A"; ">^A"]
-//        ]
-//    ]
-//]
-
-
-// All codes ends on A so we can consider them separately.
-// To press the final A, the depressurized robot would need to be on the A, 
-// and the irradiated robot on A and the frozen robot on A
-// So essentially each code starts with all robots on A
-
-
-//allDirPadPaths '^' '<'
-//allDirPadPaths 'A' '<'
-//allDirPadPaths 'v' 'A'
-//allDirPadPaths '<' 'A'
-//allDirPadPaths '^' '>'
-
-//allNumPadPaths 'A' '7' |> Seq.toArray |> Dump
-//allNumPadPaths '0' '7' |> Seq.toArray |> Dump
-//allNumPadPaths '3' 'A' |> Seq.toArray |> Dump
-
-//allNumPadPaths '0' '2' |> Seq.toArray |> Dump
-//allNumPadPaths '2' '9' |> Seq.toArray |> Dump
-//allNumPadPaths '9' 'A' |> Seq.toArray |> Dump
-
-
-//#r "nuget: quikgraph"
-//#r "nuget: quikgraph.GraphViz"
-
-//open QuikGraph
-//open QuikGraph.Graphviz
-
-//let g = new QuikGraph.AdjacencyGraph<char, TaggedEdge<char, Direction>>()
-//g.AddVertexRange([0..9] |> Seq.map (fun i -> (('0' |> int) + i) |> char));
-//g.AddVertex('A')
-//g.AddEdge(new TaggedEdge<char, Direction>('0','A',Right))
-//g.AddEdge(new TaggedEdge<char, Direction>('A','0',Left))
-//g.AddEdge(new TaggedEdge<char, Direction>('0','2',Up))
-//g.AddEdge(new TaggedEdge<char, Direction>('2','0',Down))
-//g.AddEdge(new TaggedEdge<char, Direction>('A','3', Up))
-//g.AddEdge(new TaggedEdge<char, Direction>('3','A', Down))
-//g.AddEdge(new TaggedEdge<char, Direction>('1','2', Right))
-//g.AddEdge(new TaggedEdge<char, Direction>('2','1', Left))
-//g.AddEdge(new TaggedEdge<char, Direction>('1','4', Up))
-//g.AddEdge(new TaggedEdge<char, Direction>('4','1', Down))
-////g.AddEdge(new Edge<char>('1','4'))
-////g.AddEdge(new Edge<char>('2','3'))
-////g.AddEdge(new Edge<char>('2','5'))
-////g.AddEdge(new Edge<char>('3','6'))
-////g.AddEdge(new Edge<char>('4','5'))
-////g.AddEdge(new Edge<char>('4','7'))
-////g.AddEdge(new Edge<char>('5','6'))
-////g.AddEdge(new Edge<char>('5','8'))
-////g.AddEdge(new Edge<char>('6','9'))
-////g.AddEdge(new Edge<char>('7','8'))
-////g.AddEdge(new Edge<char>('7','9'))
-
-
-//let algo = QuikGraph.Algorithms.ShortestPath.DijkstraShortestPathAlgorithm(g, fun edge -> 1.0)
-
-//let vd = new QuikGraph.Algorithms.Observers.VertexDistanceRecorderObserver<char, TaggedEdge<char, Direction>>(fun edge -> 1.0)
-//vd.Attach(algo)
-
-//let vp = new QuikGraph.Algorithms.Observers.VertexPredecessorRecorderObserver<char, TaggedEdge<char, Direction>>()
-//vp.Attach(algo)
-
-//algo.Compute('A')
-//algo.GetDistances() |> Seq.toArray |> Dump
-
-//for kvp in vd.Distances do
-//    Console.WriteLine("Distance from root to node {0} is {1}", kvp.Key, kvp.Value);
-
-//for kvp in vp.VerticesPredecessors do
-//    Console.WriteLine("If you want to get to {0} you have to enter through the in edge {1}", kvp.Key, kvp.Value );
-
-
-//// File.WriteAllText(@"c:\temp\day21\num_keypad.dot", g.ToGraphviz())
-
-//QuikGraph.Algorithms.ShortestPath.
-
+solve2 "Day21.txt"
