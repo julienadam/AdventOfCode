e78a7a74d8b85b502d30e4adf16d02d77c0da578
Julien Adam <julien.adam@arcodia.fr>
2025-11-30 17:41:45 +0100

    2019 Day 03 part 2
    dirty but runs under 200ms

--- Puzzles/2019/Day03.fsx
+++ Puzzles/2019/Day03.fsx
@@ -83,7 +83,59 @@ solve1 "Day03_sample2.txt" // 159
 solve1 "Day03_sample3.txt" // 135
 solve1 "Day03.txt"
 
+let stepsTo target segments =
+    let stepsOnPath (instruction:string) =
+        let steps = instruction.Substring(1) |> int
+        match instruction[0] with
+        | 'U' -> List.replicate steps (-1,0)
+        | 'D' -> List.replicate steps (1,0)
+        | 'R' -> List.replicate steps (0,1)
+        | 'L' -> List.replicate steps (0,-1)
+        | x -> failwithf $"not a valid direction {x}"
+    
+    let rec walk remainingStepsOnPath remainingInstructions steps (r,c) =
+        if (r,c) = target then steps
+        else
+            match remainingStepsOnPath with
+            | [] ->
+                match remainingInstructions with
+                | [] -> failwithf $"{target} not found on path"
+                | head::tail -> walk (stepsOnPath head) tail steps (r,c)
+            | (sr,sc)::remaining ->
+                walk remaining remainingInstructions (steps+1) (r+sr, c+sc)
+
+    walk [] segments 0 (0,0)
+    // let mutable steps = 0
+    // let mutable pos = (0,0)
+    
+#r "nuget: FSharp.Collections.ParallelSeq"
+
+open FSharp.Collections.ParallelSeq
+
 let solve2 input =
-    ()
+    let wires1, wires2 = getInput input
+    let intersections =
+        SeqEx.crossproduct wires1 wires2
+        |> Seq.collect (fun (s1, s2) ->
+            match s1, s2 with
+            | Horizontal a, Horizontal b -> tryIntersectHH a b
+            | Vertical a, Vertical b -> tryIntersectVV a b
+            | Horizontal a, Vertical b -> tryIntersectVH b a
+            | Vertical a, Horizontal b -> tryIntersectVH a b
+            )
+        |> Seq.filter (fun (r,c)-> r <> 0 && c <> 0)
+    
+    let instructions1, instructions2 =
+        File.ReadAllLines(getInputPath2019 input)
+        |> Array.map (fun l -> l |> ssplit "," |> Array.toList)
+        |> tupleize2
+        
+    intersections
+    |> PSeq.map (fun (r,c) -> stepsTo (r,c) instructions1 + stepsTo (r,c) instructions2)
+    |> Seq.min
+
+solve2 "Day03_sample1.txt" // 30
+solve2 "Day03_sample2.txt" // 610
+solve2 "Day03_sample3.txt" // 410
 
 solve2 "Day03.txt"
