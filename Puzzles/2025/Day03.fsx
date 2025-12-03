24b130aaabb77b692fc9a383f84cb0665cb6f88c
Julien Adam <julien.adam@arcodia.fr>
2025-12-03 11:52:55 +0100

    Refactoring to use maxBy and arrays

--- Puzzles/2025/Day03.fsx
+++ Puzzles/2025/Day03.fsx
@@ -16,22 +16,21 @@ let getInput name =
     File.ReadAllLines(getInputPath2025 name)
     |> Seq.map mapBank
 
-let findBestJoltage length (bank:int64[]) =
-    let rec findRec (remainingBatteriesInBank:int64 list) numberLeft (joltage:int64) =
+let findBestJoltage length (bank:int64 array) =
+    let rec findRec (remainingBatteriesInBank:int64 array) numberLeft (joltage:int64) =
         if numberLeft = 0 then
             joltage
         else
-            let numCandidates = (remainingBatteriesInBank |> List.length) - numberLeft + 1
+            let numCandidates = (remainingBatteriesInBank |> Array.length) - numberLeft + 1
             let indexOfBestBattery =
                 remainingBatteriesInBank
                 |> Seq.take numCandidates
                 |> Seq.indexed
-                |> Seq.sortByDescending snd // sort is stable and will always return the items in order if duplicates are found
-                |> Seq.head
+                |> Seq.maxBy snd
                 |> fst
             let bestBattery = remainingBatteriesInBank[indexOfBestBattery] // |> Dump
-            findRec (remainingBatteriesInBank |> List.skip (indexOfBestBattery + 1)) (numberLeft - 1) (joltage + (pow10 (numberLeft - 1)) * bestBattery)
-    findRec (bank |> Array.toList) length 0 // |> Dump
+            findRec (remainingBatteriesInBank |> Array.skip (indexOfBestBattery + 1)) (numberLeft - 1) (joltage + (pow10 (numberLeft - 1)) * bestBattery)
+    findRec bank length 0 // |> Dump
 
 let solve1 input =
     getInput input
