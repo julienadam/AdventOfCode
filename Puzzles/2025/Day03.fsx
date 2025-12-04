f7321fe3eface4499e70988d0af31f3a7f42b2c0
Julien Adam <julien.adam@arcodia.fr>
2025-12-04 13:54:31 +0100

    Optimized Day 03
    Keep start and end indexes in the original arrays instead of creating a
    new array every recursion step.

--- Puzzles/2025/Day03.fsx
+++ Puzzles/2025/Day03.fsx
@@ -3,6 +3,7 @@
 #load "../../Tools/MathEx.fs"
 #r "nuget: NFluent"
 open System
+open System.Diagnostics
 open System.IO
 open AdventOfCode
 open Checked
@@ -16,25 +17,29 @@ let getInput name =
     File.ReadAllLines(getInputPath2025 name)
     |> Seq.map mapBank
 
-let findBestJoltage length (bank:int64 array) =
-    let rec findRec (remainingBatteriesInBank:int64 array) numberLeft (joltage:int64) =
+let findBiggestLeftMostInRange start length (batteries:int64[]) =
+    let mutable max = 0L
+    let mutable index = -1
+    for i = start to start + length - 1 do
+        if batteries[i] > max then
+            max <- batteries[i]
+            index <- i
+    index
+
+let findBestJoltageV2 length (bank:int64 array) =
+    let rec findRec startPos numberLeft (joltage:int64) =
         if numberLeft = 0 then
             joltage
         else
-            let numCandidates = (remainingBatteriesInBank |> Array.length) - numberLeft + 1
-            let indexOfBestBattery =
-                remainingBatteriesInBank
-                |> Seq.take numCandidates
-                |> Seq.indexed
-                |> Seq.maxBy snd
-                |> fst
-            let bestBattery = remainingBatteriesInBank[indexOfBestBattery] // |> Dump
-            findRec (remainingBatteriesInBank |> Array.skip (indexOfBestBattery + 1)) (numberLeft - 1) (joltage + (pow10 (numberLeft - 1)) * bestBattery)
-    findRec bank length 0 // |> Dump
+            let numCandidates = (bank |> Array.length) - startPos - numberLeft + 1
+            let indexOfBestBattery = findBiggestLeftMostInRange startPos numCandidates bank
+            let bestBattery = bank[indexOfBestBattery]
+            findRec (indexOfBestBattery+1) (numberLeft - 1) (joltage + (pow10 (numberLeft - 1)) * bestBattery)
+    findRec 0 length 0
 
 let solve1 input =
     getInput input
-    |> Seq.map (findBestJoltage 2)
+    |> Seq.map (findBestJoltageV2 2)
     |> Seq.sum
 
 Check.That(solve1 "Day03_sample1.txt").IsEqualTo(357)
@@ -42,8 +47,10 @@ solve1 "Day03.txt"
 
 let solve2 input =
     getInput input
-    |> Seq.map (findBestJoltage 12)
+    |> Seq.map (findBestJoltageV2 12)
     |> Seq.sum
 
 Check.That(solve2 "Day03_sample1.txt").IsEqualTo(3121910778619L)
+let sw = Stopwatch.StartNew()
 solve2 "Day03.txt"
+printfn $"{sw}"
