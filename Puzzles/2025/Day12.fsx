549291acd776cb637e28210fad62bdb018f7cf95
Julien Adam <julien.adam@arcodia.fr>
2025-12-12 20:20:15 +0100

    2025 Day 12 done
    lol, the puzzle was designed to be done by just eliminating obviously
    impossible situations (i.e. more shape cells to fill than there was
    total cells available). And did absolutely not require to actually fit
    the shapes.
    
    Anyway, I learned a lot about packing, exact cover and so forth

--- Puzzles/2025/Day12.fsx
+++ Puzzles/2025/Day12.fsx
@@ -3,6 +3,8 @@
 #load "../../Tools/Array2DTools.fs"
 #r "nuget: NFluent"
 
+let debug = false
+
 open System
 open System.IO
 open System.Linq
@@ -55,19 +57,18 @@ let enumerateValidPlacements  boardRows boardCols piece=
     }
     
 #r "nuget: DlxLib"
-
 #load "../../Tools/SeqEx.fs"
-
 open DlxLib
 
-let canPlacePiecesOnBoard (pieces:int[,] array) (boardHeight, boardWidth) =
+let canPlacePiecesOnBoard (pieces:int[,] array) (boardRows, boardCols) =
     let printer i = if i = 0 then '.' else '#'
-    let inline rcToDlxCol r c rowSize = r * rowSize + c
-
+    
     // We're going to use an existing implementation of Knuth's Algorithm X Dancing Links (DLX)
     // For that we need a matrix with those columns :
-    // - one column for each square in the target grid (e.g. 12*5 would result in 60 columns
     // - one column for each individual piece (if the same piece is present 2 times, add 2 columns etc.) 
+    //  - these columns will be primary, as we want to be able to place all pieces
+    // - one column for each square in the target grid (e.g. 12*5 would result in 60 columns
+    //   - these columns will be secondary, as be don't care if some are not filled
     // Each row will represent one variant of each piece (combination of rotations and flip)
     // at one valid placement in the grid
     
@@ -76,69 +77,84 @@ let canPlacePiecesOnBoard (pieces:int[,] array) (boardHeight, boardWidth) =
         pieces
         |> Seq.indexed
         |> Seq.collect (fun (id, p) ->
-            SeqEx.crossproduct (p |> enumerateValidPlacements boardHeight boardWidth) (p |> getAllDistinctRotationsAndMirrors)
+            SeqEx.crossproduct (p |> enumerateValidPlacements boardRows boardCols) (p |> getAllDistinctRotationsAndMirrors)
             |> Seq.map (fun p -> id, p))
-        // |> Seq.toArray |> Dump
+        |> Seq.toArray // |> Dump
+        
+    printfn $"Found {placedVariants.Length} variants"
+    
+    let inline rcToDlxCol r c = r * boardCols + c
 
     // Build the DLX matrix
     let matrix =
         placedVariants
         |> Seq.map (fun (id, ((r,c), variant)) ->
             // Initialize an empty row
-            let mutable row = Array.zeroCreate<int> (boardHeight*boardWidth+pieces.Length)
+            let mutable row = Array.zeroCreate<int> (boardRows*boardCols+pieces.Length)
+            // Mark the piece's id
+            row[id] <- 1
+            
             // For each filled square in the piece, mark the corresponding column with a 1 
             for offR = 0 to variant |> maxR do
                 for offC = 0 to variant |> maxC do
                     if variant[offR,offC] = 1 then
-                        row[rcToDlxCol (r+offR) (c+offC) boardWidth] <- 1
-            // Mark the piece's id
-            row[boardHeight * boardWidth + id] <- 1
+                        row[pieces.Length + rcToDlxCol (r+offR) (c+offC)] <- 1
             row
             )
-        |> array2D |> printGridCustom printer
+        |> array2D
+    if debug then (matrix |> printGridCustom printer) |> ignore
     
     // Try to solve the DLX matrix
     let dlx = Dlx()
-    match dlx.Solve(matrix) |> Seq.tryHead with
+    match dlx.Solve(matrix, pieces.Length) |> Seq.tryHead with
     | Some solution ->
+        if debug then printfn $"solution found : %A{solution.RowIndexes |> Seq.toArray}"
         true
-        // printfn $"solution found : %A{solution.RowIndexes |> Seq.toArray}"
     | _ ->
         false
-        //printfn "No solution found"
 
 let square2_2 = Array2D.create 2 2 1
 Check.That(canPlacePiecesOnBoard [| square2_2 |] (2,4)).IsEqualTo(true)
 Check.That(canPlacePiecesOnBoard [| square2_2;square2_2 |] (2,4)).IsEqualTo(true)
 Check.That(canPlacePiecesOnBoard [| square2_2; square2_2; square2_2 |] (2,4)).IsEqualTo(false)
 
-let solve1 input =
-    let regions, pieces = getInput input
-    
-    let (rows, cols), numPieces  = regions |> Seq.head
-    
+let solveRegion numRows numCols numPieces (pieces:int[,] array) =
     let allPieces =
         numPieces
         |> Seq.indexed
         |> Seq.collect (fun (idx, num) -> Seq.replicate num pieces[idx])
         |> Seq.toArray
-    
-    canPlacePiecesOnBoard allPieces (rows, cols)
-    
-solve1 "Day12_sample2.txt"
+     
+    canPlacePiecesOnBoard allPieces (numRows, numCols)
 
-
-    // let p1 = pieces[5]
-    // enumerateValidPlacements p1 8 8
-    // |> Seq.iter (fun (r,c) ->
-    //     printfn ""
-    //     let board = Array2D.zeroCreate<int> 8 8
-    //     Array2D.blit p1 0 0 board r c (p1 |> Array2D.length1) (p1 |> Array2D.length2)
-    //     board |> printGridCustom printer |> ignore)
-    //  
-    // getAllDistinctRotationsAndMirrors p1
-    // |> Seq.iter (fun variation ->
-    //     printfn ""
-    //     variation |> printGridCustom printer |> ignore
-    //     )
+let isImpossible numRows numCols (numPieces:int array) (pieces:int[,] array) =
+    let totalCellsToPlace =
+        pieces |> Seq.indexed |> Seq.sumBy (fun (i,p) ->
+            let cells = p |> countWhere (fun _ _ c -> c = 1)
+            cells * numPieces[i]
+        )
+    printfn $"{numRows * numCols} grid. Need to place {totalCellsToPlace}."
+    numRows * numCols < totalCellsToPlace
     
+let solve1 input =
+    let regions, pieces = getInput input
+    let mutable reg = 0
+    regions
+    |> Seq.filter(fun ((rows, cols), numPieces) ->
+        reg <- reg + 1
+        if isImpossible rows cols numPieces pieces then
+            printfn $"Region {reg} is impossible, too many cells, not enough available"
+            false
+        else
+            printfn $"Solving region {reg}"
+            // solveRegion rows cols numPieces pieces)
+            true)
+    |> Seq.length
+    
+Check.That(solve1 "Day12_can_place_2_square_blocs.txt").IsEqualTo(1)
+Check.That(solve1 "Day12_sample1.txt").IsEqualTo(2)
+    
+// So, the shapes were designed to fit if there was enough room ...
+// no need to actually solve the problem
+
+solve1 "Day12.txt"
